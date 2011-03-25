fb7fde43d653157ceba44ec112e5e916222a6d37
Daniel Cukier <danicuki@gmail.com>
2011-03-25 14:15:49 -0300

    commit inicial do component hello world para petals

---
+++ petals-se-hello_component/src/main/java/org/ow2/petals/se/hello_component/ServiceEngine.java
@@ -0,0 +1,79 @@
+/**
+ * PETALS - PETALS Services Platform.
+ * Copyright (c) 2007 EBM Websourcing, http://www.ebmwebsourcing.com/
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.ow2.petals.se.hello_component;
+
+import javax.jbi.JBIException;
+
+import org.ow2.petals.component.framework.se.AbstractServiceEngine;
+
+/**
+ * The petals-se-hello_component Service Engine.
+ * 
+ * <br>
+ * <b>NOTE : </b>This class has to be used only if the component developer wants
+ * to customize the main component class. In general, using the
+ * org.objectweb.petals.component.framework.se.DefaultServiceEngine class is
+ * enough. If so, change the value in the JBI descriptor file.
+ * 
+ * @author
+ * 
+ */
+public class ServiceEngine extends AbstractServiceEngine {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ow2.petals.component.framework.AbstractComponent#doInit()
+     */
+    @Override
+    protected void doInit() throws JBIException {
+        // TODO implement the specific initialisation of the component
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ow2.petals.component.framework.AbstractComponent#doStart()
+     */
+    @Override
+    protected void doStart() throws JBIException {
+        // TODO implement the specific start of the component
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ow2.petals.component.framework.AbstractComponent#doStop()
+     */
+    @Override
+    protected void doStop() throws JBIException {
+        // TODO implement the specific stop of the component
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.ow2.petals.component.framework.AbstractComponent#doShutdown()
+     */
+    @Override
+    protected void doShutdown() throws JBIException {
+        // TODO implement the specific shutdown of the component
+    }
+}
