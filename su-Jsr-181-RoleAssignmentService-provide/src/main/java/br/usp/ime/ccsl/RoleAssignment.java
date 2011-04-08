da1291d50b1ae47f1b9f73e1e1c360d8fefb15c6
Eduardo Hideo Kuroda <eduardohideo@gmail.com>
2011-04-08 16:13:30 -0300

    create RoleAssignment test

--- su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/RoleAssignment.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/RoleAssignment.java
@@ -1,25 +1,38 @@
 package br.usp.ime.ccsl;
 
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
 
 /**
  * Here is a sample JAX-WS implementation.
  * <p>
- * For more information about JAX-WS, please visit 
+ * For more information about JAX-WS, please visit
  * <b>https://jax-ws.dev.java.net/jax-ws-ea3/docs/annotations.html</b>.
  * </p>
  */
-@WebService( serviceName="RoleAssignment", targetNamespace="http://ccsl.ime.usp.br", portName="RoleAssignmentPort" )
+@WebService(serviceName = "RoleAssignment", targetNamespace = "http://ccsl.ime.usp.br", portName = "RoleAssignmentPort")
 public class RoleAssignment {
 
-	@WebMethod(operationName="assignRole")
-	@WebResult(name="result")
-	public int assignRole(@WebParam(name="uri") String uri, @WebParam(name="roleName") String roleName) {
-		RoleManager roleManager = RoleManager.getInstance();
-		roleManager.assignRole(uri, roleName);
-		return 0;
-	}
+    @WebMethod(operationName = "assignRole")
+    public void assignRole(@WebParam(name = "uri") String uri, @WebParam(name = "roleName") String roleName) {
+	RoleManager roleManager = RoleManager.getInstance();
+	roleManager.assignRole(uri, roleName);
+    }
+
+    @WebMethod(operationName = "get")
+    public List<String> get(@WebParam(name = "roleName") String roleName) {
+	RoleManager roleManager = RoleManager.getInstance();
+	return roleManager.getUriList(roleName);
+    }
+    
+    public static void main(String[] args){
+        // create and publish an endpoint
+        RoleAssignment calculator = new RoleAssignment();
+        Endpoint endpoint = Endpoint.publish("http://localhost:8080/roleAssignment", calculator);        
+    }
+
 }
--- su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/RoleManager.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/RoleManager.java
@@ -1,41 +1,44 @@
 package br.usp.ime.ccsl;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+import edu.emory.mathcs.backport.java.util.Collections;
 
 public class RoleManager {
 
-	private static RoleManager instance;
-	
-	Map<String, List<String>> uriRole;
-	
-	private RoleManager() {
-		uriRole = new HashMap<String, List<String>>();
-	}
-	
-	public static RoleManager getInstance() {
-		if (instance == null) {
-			instance = new RoleManager();
-		}
-		return instance;
-	}
-	
-	public void assignRole(String uri, String roleName) {
-		List<String> uriList = uriRole.get(roleName);
-		if (uriList == null) {
-			uriList = new ArrayList<String>();
-			uriRole.put(roleName, uriList);
-		}
-		uriList.add(uri);
-	}
-	
-	public List<String> getUriList(String roleName) {
-		List<String> uriList = uriRole.get(roleName);
-		if (uriList == null) uriList = new ArrayList<String>();
-		return uriList;
-	}
-}
+    private static RoleManager instance;
 
+    ConcurrentMap<String, List<String>> uriRole;
+
+    private RoleManager() {
+	uriRole = new ConcurrentHashMap<String, List<String>>();
+    }
+
+    public static RoleManager getInstance() {
+	if (instance == null) {
+	    instance = new RoleManager();
+	}
+	return instance;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void assignRole(String uri, String roleName) {
+	List<String> uriList = uriRole.get(roleName);
+	if (uriList == null) {
+	    uriList = new ArrayList<String>();
+	    List<String> threadSafeUriList = Collections.synchronizedList(uriList);
+	    uriRole.put(roleName, threadSafeUriList);
+	}
+	uriList.add(uri);
+    }
+
+    public List<String> getUriList(String roleName) {
+	List<String> uriList = uriRole.get(roleName);
+	if (uriList == null)
+	    uriList = new ArrayList<String>();
+	return uriList;
+    }
+}
