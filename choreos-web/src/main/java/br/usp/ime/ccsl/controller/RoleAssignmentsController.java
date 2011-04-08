768cc5c838ea00a23856e545bbcbb78a30e9dc30
Daniel Cukier <danicuki@gmail.com>
2011-04-08 13:42:19 -0300

    finished the vraptor prototype

--- choreos-web/src/main/java/br/usp/ime/ccsl/controller/RoleAssignmentsController.java
+++ choreos-web/src/main/java/br/usp/ime/ccsl/controller/RoleAssignmentsController.java
@@ -3,24 +3,26 @@ package br.usp.ime.ccsl.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.usp.ime.ccsl.model.RoleAssignment;
 
 @Resource
 public class RoleAssignmentsController {
 
+    private static List<RoleAssignment> list = new ArrayList<RoleAssignment>();
+
     public void form() {
-        // code that loads data for checkboxes, selects, etc
+	// code that loads data for checkboxes, selects, etc
     }
 
-    
-    public void add(RoleAssignment roleAssignment){
+    public RoleAssignment add(RoleAssignment roleAssignment) {
+	list.add(roleAssignment);
 	System.out.println("adicionou " + roleAssignment.getUri() + " no role " + roleAssignment.getRole());
+	return roleAssignment;
     }
     
     public List<RoleAssignment> list() {
-        return new ArrayList<RoleAssignment>();
+	return list;
     }
 
 }
--- choreos-web/src/main/webapp/index.jsp
+++ choreos-web/src/main/webapp/index.jsp
@@ -1,3 +1,3 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
 
-<h1> HELLO choreos !! </h1>
+<h1> HELLO choreos!</h1>
