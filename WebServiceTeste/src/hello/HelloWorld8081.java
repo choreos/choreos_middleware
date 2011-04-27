92f3672b2c710908b3379658777e7458941e9a59
felps <fpontes100@hotmail.com>
2011-04-27 13:26:52 -0300

    Including SOAPUI WSClient to allow for dynamic creation of Web Service Clients Pending: Include SOAPUI in POM.XML and make it work.

--- WebServiceTeste/src/hello/HelloWorld8081.java
+++ WebServiceTeste/src/hello/HelloWorld8081.java
@@ -2,36 +2,40 @@ package hello;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
-
 
 @WebService
 public class HelloWorld8081 {
 
-	private String instanceName;
+    private String instanceName;
+    private int reqCount = 0;
+
+    public HelloWorld8081(String name) {
+	// TODO Auto-generated constructor stub
+	this.instanceName = name;
+    }
+
+    @WebMethod
+    @WebResult(name="greeting")
+    public String sayHello(@WebParam(name = "yourName") String param) {
+	System.out.println(this.instanceName + ": Requisition number: " + reqCount++);
 	
-	public HelloWorld8081(String name) {
-		// TODO Auto-generated constructor stub
-		this.instanceName = name;
+	return "Hello from " + instanceName + " " + param;
+    }
+
+    public static void main(String[] args) {
+	HelloWorld8081[] service = new HelloWorld8081[4];
+
+	for (int i = 8081; i <= 8084; i++) {
+	    service[i - 8081] = new HelloWorld8081(i + "");
+
+	    Endpoint endpoint = Endpoint.create(service[i - 8081]);
+
+	    endpoint.publish("http://localhost:" + i + "/hello");
+	    System.out.println("Serviço disponibilizado na porta " + i);
 	}
-	
-	@WebMethod
-	public String sayHello(@WebParam(name="yourName") String param) {
-		return "Hello from " + instanceName + " " + param; 
-	}
-	
-	public static void main(String[] args) {
-		HelloWorld8081[] service = new HelloWorld8081[4];
-		
-		for(int i = 8081; i<=8084; i++){
-	        service[i-8081] = new HelloWorld8081(i+"");
-	        
-	        Endpoint endpoint = Endpoint.create(service[i-8081]);
-	        
-	        endpoint.publish("http://localhost:"+i+"/hello");
-	        System.out.println("Serviço disponibilizado na porta "+i);
-		}
-		
-	}
+
+    }
 }
--- petals-bc-loggedsoap/src/main/java/org/ow2/petals/bc/loggedsoap/listeners/JBIListener.java
+++ petals-bc-loggedsoap/src/main/java/org/ow2/petals/bc/loggedsoap/listeners/JBIListener.java
@@ -38,21 +38,20 @@ import org.ow2.petals.component.framework.util.UtilFactory;
  */
 public class JBIListener extends AbstractJBIListener {
 
-	private static final Logger log = Logger.getLogger(JBIListener.class.getName());
-	
-	
-	
-    @Override
-	public Logger getLogger() {
-		return log;
-	}
+    private static final Logger log = Logger.getLogger(JBIListener.class
+	    .getName());
 
-	/**
+    @Override
+    public Logger getLogger() {
+	return log;
+    }
+
+    /**
      * Creates a new instance of {@link JBIListener}
      * 
      */
     public JBIListener() {
-        super();
+	super();
     }
 
     /*
@@ -61,37 +60,49 @@ public class JBIListener extends AbstractJBIListener {
      * @see org.ow2.petals.component.framework.listener.AbstractJBIListener
      */
     public boolean onJBIMessage(final Exchange exchange) {
-        try {
-            if (component.getNativeWsdl() != null
-                    && UtilFactory.getWSDLUtil().isDescriptionContaining(
-                component.getNativeWsdl().getDescription(), exchange.getEndpoint())) {
-                getLogger().info("exchange to process internally received");
-            } else {
-                getLogger().info("exchange to process with the SU manager received");
-            }
-            getLogger().info("exchange endpoint name: " + exchange.getEndpointName());
-            getLogger().info("exchange operation name: " + exchange.getOperationName());
-            if (exchange.getFault() != null) { 
-                if (UtilFactory.getExchangeUtil().isPetalsException(exchange.getFault())) { 
-                    getLogger().info("exchange technical fault message content: " +
-                        UtilFactory.getSourceUtil().createString(exchange.getFault().getContent())); 
-                } else {
-                    getLogger().info( "exchange business fault message content: " +
-                        UtilFactory.getSourceUtil().createString(exchange.getFault().getContent()));
-                }
-            } else {
-                getLogger().info( "exchange in message content: " +
-                    UtilFactory.getSourceUtil().createString(exchange.getInMessage().getContent()));
-            }
-            if(exchange.isInOutPattern() || exchange.isInOptionalOutPattern()) {
-                exchange.getOutMessage().setContent(
-                        UtilFactory.getSourceUtil().createSource("<helloworld>Hello World!</helloworld>"));
-            }
-        } catch (Exception e) {
-        	getLogger().log(Level.SEVERE, "error dumbass", e);
-            exchange.setError(new Exception("An error occurs during the treatment of an exchange: " + e.getMessage()));
-        }
-        return true;
+	try {
+	    if (component.getNativeWsdl() != null
+		    && UtilFactory.getWSDLUtil().isDescriptionContaining(
+			    component.getNativeWsdl().getDescription(),
+			    exchange.getEndpoint())) {
+		getLogger().info("exchange to process internally received");
+	    } else {
+		getLogger().info(
+			"exchange to process with the SU manager received");
+	    }
+	    getLogger().info("exchange endpoint name: " + exchange.getEndpointName());
+	    getLogger().info("exchange operation name: " + exchange.getOperationName());
+	    System.out.println("teste-muito-antes");
+	    if (exchange.getFault() != null) {
+		System.out.println("teste-out");
+		if (UtilFactory.getExchangeUtil().isPetalsException(exchange.getFault())) {
+		    System.out.println("teste-in");
+		    getLogger().info(
+			    "exchange technical fault message content: "
+				    + UtilFactory.getSourceUtil().createString(exchange.getFault().getContent()));
+		} else {
+		    System.out.println("teste-antes");
+		    getLogger().info(
+			    "exchange business fault message content: "
+				    + UtilFactory.getSourceUtil().createString(exchange.getFault().getContent()));
+		    System.out.println("teste-depois");
+		}
+	    } else {
+		getLogger().info(
+			"exchange in message content: "
+				+ UtilFactory.getSourceUtil().createString(exchange.getInMessage().getContent()));
+	    }
+	    if (exchange.isInOutPattern() || exchange.isInOptionalOutPattern()) {
+		exchange.getOutMessage().setContent(
+			UtilFactory.getSourceUtil().createSource("<helloworld>Hello World!</helloworld>"));
+	    }
+	    
+	    getLogger().info("Log generated successfully");
+	} catch (Exception e) {
+	    exchange.setError(new Exception(
+		    "An error occurs during the treatment of an exchange: "
+			    + e.getMessage()));
+	}
+	return true;
     }
 }
-
