2ee5c7ecd1413e00729af9f50fb12af00b33b9d9
Thiago Colucci <ticolucci@gmail.com>
2011-04-27 15:02:42 -0300

    fixing Log4J mvn problem Thiago & Edu

--- su-Jsr-181-RoleAssignmentService-provide/src/test/java/br/usp/ime/ccsl/choreos/middleware/proxy/ProxyTest.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/test/java/br/usp/ime/ccsl/choreos/middleware/proxy/ProxyTest.java
@@ -1,9 +1,14 @@
 package br.usp.ime.ccsl.choreos.middleware.proxy;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.xmlbeans.XmlException;
 import org.junit.Test;
+
+import br.usp.ime.ccsl.choreos.middleware.exceptions.FrameworkException;
+import br.usp.ime.ccsl.choreos.middleware.exceptions.WSDLException;
 
 import static org.junit.Assert.*;
 
@@ -15,7 +20,18 @@ public class ProxyTest {
 		Proxy proxy = new Proxy("roleName");
 		
 		List<WSClient>  expected = new ArrayList<WSClient>();
-		WSClient ws = new WSClient("http://localhost:8081/hello?wsdl");
+		WSClient ws = null;
+		try {
+			ws = new WSClient("http://localhost:8081/hello?wsdl");
+		} catch (WSDLException e) {
+			e.printStackTrace();
+		} catch (XmlException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (FrameworkException e) {
+			e.printStackTrace();
+		}
 		
 		expected.add(ws);
 		proxy.addService(ws);
