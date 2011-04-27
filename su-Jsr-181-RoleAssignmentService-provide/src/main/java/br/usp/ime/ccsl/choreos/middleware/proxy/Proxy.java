ea0752a0a509011507cb8f5c09252c6d6c91951a
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2011-04-27 16:05:15 -0300

    Added a proxy test with mock(ito).

--- su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/choreos/middleware/proxy/Proxy.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/choreos/middleware/proxy/Proxy.java
@@ -1,21 +1,20 @@
 package br.usp.ime.ccsl.choreos.middleware.proxy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Proxy {
+    private List<WSClient> wsList;
 
-	public Proxy(String roleName) {
-		// TODO Auto-generated constructor stub
-	}
+    public Proxy(String roleName) {
+	wsList = new ArrayList<WSClient>();
+    }
 
-	public List<WSClient> getWebServiceList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public List<WSClient> getWebServiceList() {
+	return wsList;
+    }
 
-	public void addService(WSClient ws) {
-		// TODO Auto-generated method stub
-		
-	}
-
+    public void addService(WSClient ws) {
+	wsList.add(ws);
+    }
 }
--- su-Jsr-181-RoleAssignmentService-provide/src/test/java/br/usp/ime/ccsl/choreos/middleware/proxy/ProxyTest.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/test/java/br/usp/ime/ccsl/choreos/middleware/proxy/ProxyTest.java
@@ -14,44 +14,46 @@ import br.usp.ime.ccsl.choreos.middleware.exceptions.FrameworkException;
 import br.usp.ime.ccsl.choreos.middleware.exceptions.WSDLException;
 
 import static org.junit.Assert.*;
-import  br.usp.ime.ccsl.choreos.middleware.proxy.genericHelloWorldService.*;
+import br.usp.ime.ccsl.choreos.middleware.proxy.genericHelloWorldService.*;
+
+import static org.mockito.Mockito.*;
 
 public class ProxyTest {
 
-	private HelloWorld8081 webService = new HelloWorld8081("testing server");
-	
-	@Before
-	public void setUp() {
-		Endpoint endpoint = Endpoint.create(webService);
+    private HelloWorld8081 webService = new HelloWorld8081("testing server");
 
-	    endpoint.publish("http://localhost:8081/hello");
+    @Before
+    public void setUp() {
+	Endpoint endpoint = Endpoint.create(webService);
 
-	}
+	endpoint.publish("http://localhost:8081/hello");
+
+    }
+
+    @Test
+    public void shouldAddOneWebService() {
+
+	Proxy proxy = new Proxy("roleName");
+
+	List<WSClient> expected = new ArrayList<WSClient>();
+	WSClient wsMock = mock(WSClient.class);
 	
-	@Test
-	public void shouldHaveManyWebServices(){
-		
-		Proxy proxy = new Proxy("roleName");
-		
-		List<WSClient>  expected = new ArrayList<WSClient>();
-		WSClient ws = null;
-		try {
-			ws = new WSClient("http://localhost:8081/hello?wsdl");
-		} catch (WSDLException e) {
-			e.printStackTrace();
-		} catch (XmlException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (FrameworkException e) {
-			e.printStackTrace();
-		}
-		
-		expected.add(ws);
-		proxy.addService(ws);
-		
-		assertEquals(expected, proxy.getWebServiceList());
+	try {
+	    wsMock = new WSClient("http://localhost:8081/hello?wsdl");
+	} catch (WSDLException e) {
+	    e.printStackTrace();
+	} catch (XmlException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} catch (FrameworkException e) {
+	    e.printStackTrace();
 	}
 
-	
+	expected.add(wsMock);
+	proxy.addService(wsMock);
+
+	assertEquals(expected, proxy.getWebServiceList());
+    }
+
 }
