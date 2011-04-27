019dd8a609d604725365a6534d3da271d8b0fe29
felps <fpontes100@hotmail.com>
2011-04-27 15:28:46 -0300

    Included the web server in the test. It automatically rises before the test.

--- su-Jsr-181-RoleAssignmentService-provide/src/test/java/br/usp/ime/ccsl/choreos/middleware/proxy/ProxyTest.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/test/java/br/usp/ime/ccsl/choreos/middleware/proxy/ProxyTest.java
@@ -4,16 +4,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.ws.Endpoint;
+
 import org.apache.xmlbeans.XmlException;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.usp.ime.ccsl.choreos.middleware.exceptions.FrameworkException;
 import br.usp.ime.ccsl.choreos.middleware.exceptions.WSDLException;
 
 import static org.junit.Assert.*;
+import  br.usp.ime.ccsl.choreos.middleware.proxy.genericHelloWorldService.*;
 
 public class ProxyTest {
 
+	private HelloWorld8081 webService = new HelloWorld8081("testing server");
+	
+	@Before
+	public void setUp() {
+		Endpoint endpoint = Endpoint.create(webService);
+
+	    endpoint.publish("http://localhost:8081/hello");
+
+	}
+	
 	@Test
 	public void shouldHaveManyWebServices(){
 		
