05421a7392793ec1d7d25d56fdef5ab524d338ce
Daniel Cukier <danicuki@gmail.com>
2011-05-06 16:10:48 -0300

    reformating the test and some code issues, maven site builder and checkstyle

--- ProxySoapUI/src/br/usp/ime/choreos/middleware/exceptions/InvalidOperationName.java
+++
@@ -1,7 +0,0 @@
-package br.usp.ime.choreos.middleware.exceptions;
-
-public class InvalidOperationName extends Exception {
-
-	private static final long serialVersionUID = -3562272248519682510L;
-
-}
