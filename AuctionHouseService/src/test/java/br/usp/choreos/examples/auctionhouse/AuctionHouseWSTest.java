60ce32d77a4a4a2421166ecfad8e7a30b7ccec2f
Flavio Schiavini Abe <flaviosabe@gmail.com>
2011-05-13 16:17:56 -0300

    Refactor test.

--- AuctionHouseService/src/test/java/br/usp/choreos/examples/auctionhouse/AuctionHouseWSTest.java
+++ AuctionHouseService/src/test/java/br/usp/choreos/examples/auctionhouse/AuctionHouseWSTest.java
@@ -1,19 +1,31 @@
 package br.usp.choreos.examples.auctionhouse;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.xml.ws.Endpoint;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import br.usp.ime.choreos.vv.ResponseItem;
 import br.usp.ime.choreos.vv.WSClient;
 
 public class AuctionHouseWSTest {
-    @Test
-    public void shouldPublishTheRightWS() throws Exception {
+    private WSClient wsClient;
+
+    @Before
+    public void setUp() throws Exception {
 	AuctionHouseWS auctionHouseWS = new AuctionHouseWS();
 	Endpoint endpoint = Endpoint.create(auctionHouseWS);
 	endpoint.publish("http://localhost:6166/AuctionHouseService");
 	
-	WSClient wsClient = new WSClient("http://localhost:6166/AuctionHouseService?wsdl");
-	wsClient.request("publishAuction", "test_headline", "test_description", "1");
+	wsClient = new WSClient("http://localhost:6166/AuctionHouseService?wsdl");
+    }
+    
+    @Test
+    public void firstRequestShouldReturnTheFirstId() throws Exception {
+	ResponseItem responseItem = wsClient.request("publishAuction", "test_headline", "test_description", "1");
+	int auctionId = responseItem.getChild("auctionId").getContentAsInt();
+	assertEquals(0, auctionId);
     }
 }
