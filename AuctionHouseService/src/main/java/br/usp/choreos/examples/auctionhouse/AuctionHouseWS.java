e1c566cda9c262cbb030202669b09f52365915e4
Daniel Cukier <danicuki@gmail.com>
2011-05-13 13:44:43 -0300

    starting to test auction house using wsclient

--- AuctionHouseService/src/main/java/br/usp/choreos/examples/auctionhouse/AuctionHouseWS.java
+++ AuctionHouseService/src/main/java/br/usp/choreos/examples/auctionhouse/AuctionHouseWS.java
@@ -1,32 +1,28 @@
 package br.usp.choreos.examples.auctionhouse;
 
-import javax.jws.Oneway;
+import java.math.BigDecimal;
+
 import javax.jws.WebMethod;
-import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
-/**
- * Here is a sample JAX-WS implementation.
- * <p>
- * For more information about JAX-WS, please visit
- * <b>https://jax-ws.dev.java.net/jax-ws-ea3/docs/annotations.html</b>.
- * </p>
- * 
- * @author rafaelj
- */
 @WebService(serviceName = "AuctionHouseWS", targetNamespace = "http://auctionhouse.examples.choreos.usp.br", portName = "AuctionHouseWSPort")
 public class AuctionHouseWS {
 
-    @WebMethod(operationName = "helloWorld")
-    @WebResult(name = "returnMessage")
-    public String helloWorld() {
-	return "Hello World!";
+    @WebMethod(operationName = "publishAuction")
+    @WebResult(name = "auctionId")
+    public int publishAuction(String headline, String description, String startingPrice) throws AuctionHouseException {
+	ProductInfo productInfo = new ProductInfo();
+	productInfo.setHeadline(headline);
+	productInfo.setDescription(description);
+	productInfo.setStartingPrice(new BigDecimal(startingPrice));
+
+	int auctionId = new AuctionHouse().publishAuction(productInfo);
+
+	return auctionId;
     }
 
-    @WebMethod(operationName = "listenToTheWorld")
-    @Oneway
-    public void listenToTheWorld(@WebParam(name = "message") String message) {
-	// TODO: We here illustrate a method that do not return anything.
+    public static void main(String[] args) {
+
     }
 }
--- AuctionHouseService/src/test/java/br/usp/choreos/examples/auctionhouse/AuctionHouseWSTest.java
+++ AuctionHouseService/src/test/java/br/usp/choreos/examples/auctionhouse/AuctionHouseWSTest.java
@@ -5,5 +5,10 @@ import org.junit.Test;
 public class AuctionHouseWSTest {
     @Test
     public void shouldPublishTheRightWS() throws Exception {
+	// Endpoint.publish("http://localhost:6166/AuctionHouseService", new
+	// AuctionHouseWS());
+	// WSClient ws = new
+	// WSClient("http://localhost:6166/AuctionHouseService?wsdl");
+	// ws.request("publishAuction", "teste", "teste", "");
     }
 }
