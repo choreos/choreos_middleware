883da79e749890f9c45cb6cc2a312dc637eed4aa
Thiago Colucci <ticolucci@gmail.com>
2011-05-18 16:41:51 -0300

    starting the bidder project

---
+++ BidderService/src/test/java/br/usp/choreos/examples/bidder/BidderTest.java
@@ -0,0 +1,12 @@
+package br.usp.choreos.examples.bidder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BidderTest {
+    @Test
+    public void shouldSendPlaceAnOfferToTheAuctionHouse() throws Exception {
+
+    }
+}
