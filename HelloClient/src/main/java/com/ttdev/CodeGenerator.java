9fd474e5de6542ee1ed0889c3d77ff15aa575a02
Flavio Schiavini Abe <flaviosabe@gmail.com>
2011-05-27 15:46:06 -0300

    Minor modifications in HelloClient's codeGenerator
    Pending sugestion: Create a single codeGenerator class that implements both client-side and server-side code generations

--- HelloClient/src/main/java/com/ttdev/CodeGenerator.java
+++ HelloClient/src/main/java/com/ttdev/CodeGenerator.java
@@ -1,11 +1,11 @@
 package com.ttdev;
 
-import org.apache.cxf.tools.wsdlto.*;
+import org.apache.cxf.tools.wsdlto.WSDLToJava;
 
 public class CodeGenerator {
 	public static void main(String[] args) {
 		WSDLToJava.main(new String[] { "-client", "-d", "src/main/java",
-				"-compile", "http://localhost:8081/hello?wsdl" });
+				"-compile", "http://localhost:8089/hello?wsdl" });
 		System.out.println("Done!");
 	}
 }
