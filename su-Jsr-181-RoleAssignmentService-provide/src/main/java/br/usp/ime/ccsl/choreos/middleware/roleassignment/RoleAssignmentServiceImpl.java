99e027e9b46d6d312ef3adc2447e7408c3b7f922
Daniel Cukier <danicuki@gmail.com>
2011-06-03 17:02:14 -0300

    fixed roleassignment project to run ok on petals. Now tests and consume part are missing

--- su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/choreos/middleware/roleassignment/RoleAssignmentServiceImpl.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/main/java/br/usp/ime/ccsl/choreos/middleware/roleassignment/RoleAssignmentServiceImpl.java
@@ -1,20 +1,26 @@
 package br.usp.ime.ccsl.choreos.middleware.roleassignment;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 
 import br.usp.ime.ccsl.choreos.middleware.roleassignmentservice.ArrayOfStrings;
-import br.usp.ime.ccsl.choreos.middleware.roleassignmentservice.RoleAssignmentService;
 
-@WebService(endpointInterface = "br.usp.ime.ccsl.choreos.middleware.roleassignmentservice.RoleAssignmentService")
-public class RoleAssignmentServiceImpl implements RoleAssignmentService {
+@WebService(targetNamespace = "http://roleassignment.middleware.choreos.ccsl.ime.usp.br/RoleAssignmentService", name = "RoleAssignmentService")
+public class RoleAssignmentServiceImpl {
 
     public void assignRole(RoleAssignment roleAssignment) {
         RoleManager roleManager = RoleManager.getInstance();
         roleManager.assignRole(roleAssignment);
     }
 
-    public String assignRole(String role, String uri) {
+    @WebResult(name = "responseMessage", targetNamespace = "", partName = "responseMessage")
+    @WebMethod
+    public java.lang.String assignRole(
+            @WebParam(partName = "role", name = "role", targetNamespace = "") java.lang.String role,
+            @WebParam(partName = "uri", name = "uri", targetNamespace = "") java.lang.String uri) {
         RoleAssignment roleAssignment = new RoleAssignment();
         roleAssignment.setRole(role);
         roleAssignment.setUri(uri);
@@ -22,18 +28,21 @@ public class RoleAssignmentServiceImpl implements RoleAssignmentService {
         return "Created";
     }
 
-    public ArrayOfStrings get(String roleName) {
+    @WebResult(name = "uris", targetNamespace = "", partName = "uris")
+    @WebMethod
+    public ArrayOfStrings get(
+            @WebParam(partName = "roleName", name = "roleName", targetNamespace = "") java.lang.String roleName) {
         RoleManager roleManager = RoleManager.getInstance();
         ArrayOfStrings uris = new ArrayOfStrings();
         uris.setUri(roleManager.getUriList(roleName));
-
         return uris;
     }
 
     public static void main(String[] args) {
-        // create and publish an endpoint
+        // create and publish an endpointx
         RoleAssignmentServiceImpl calculator = new RoleAssignmentServiceImpl();
         Endpoint.publish("http://localhost:18080/roleAssignmentService", calculator);
+
     }
 
 }
