f590e824729260d050ceb86e7f68c27afd9ea188
felps <fpontes100@gmail.com>
2011-06-13 19:27:53 -0300

    Added default classes to allow unit testing
    Signed-off-by: felps <fpontes100@gmail.com>

---
+++ ChoreosWebServiceProxy/src/test/java/ime/usp/br/proxy/support/classes/HelloWorld8081Impl.java
@@ -0,0 +1,9 @@
+package ime.usp.br.proxy.support.classes;
+
+public class HelloWorld8081Impl implements HelloWorld8081{
+
+public String sayHello(String param0){
+Exception e = new Exception("There is no web service active for this role");
+return null;
+}
+}
---
+++ ChoreosWebServiceProxy/src/test/java/ime/usp/br/proxy/support/classes/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.support.proxy.br.usp.ime/")
+package ime.usp.br.proxy.support.classes;
