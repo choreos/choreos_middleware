a805d50ca57c7c4d9a5057aacfb33ad127fd1a89
Daniel Cukier <danicuki@gmail.com>
2011-06-13 19:00:52 -0300

    removed acceptance test

--- choreos-web/src/test/java/br/usp/ime/ccsl/acceptance/RoleAssignmentTest.java
+++ choreos-web/src/test/java/br/usp/ime/ccsl/acceptance/RoleAssignmentTest.java
@@ -2,32 +2,32 @@ package br.usp.ime.ccsl.acceptance;
 
 import net.sourceforge.jwebunit.junit.WebTestCase;
 
-import org.junit.Test;
-
 public class RoleAssignmentTest extends WebTestCase {
     
-    
-    @Override
-    public void setUp() throws Exception {
-	super.setUp();
-	setBaseUrl("http://localhost:8089/choreos-web");
-    }
 
-    @Test
-    public void testCreateAndListRoleAssignments() {
-	beginAt("/roleAssignments/form");
-
-	clickLink("add_role_assignment");
-
-	setTextField("roleAssignment.role", "myRole");
-	setTextField("roleAssignment.uri", "myURI");
-	submit();
-
-	assertTextPresent("Service myURI registered with role myRole");
-
-	clickLink("list_role_assignments");
-
-	assertTextPresent("myRole");
-	assertTextPresent("myURI");
-    }
+    // TODO create a pipeline environment to run acceptance tests in the CI
+    // server
+    // @Override
+    // public void setUp() throws Exception {
+    // super.setUp();
+    // setBaseUrl("http://localhost:8089/choreos-web");
+    // }
+    //
+    // @Test
+    // public void testCreateAndListRoleAssignments() {
+    // beginAt("/roleAssignments/form");
+    //
+    // clickLink("add_role_assignment");
+    //
+    // setTextField("roleAssignment.role", "myRole");
+    // setTextField("roleAssignment.uri", "myURI");
+    // submit();
+    //
+    // assertTextPresent("Service myURI registered with role myRole");
+    //
+    // clickLink("list_role_assignments");
+    //
+    // assertTextPresent("myRole");
+    // assertTextPresent("myURI");
+    // }
 }
