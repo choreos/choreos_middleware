58584a61458c8a01d66583d38f4550a1f0768996
Daniel Cukier <danicuki@gmail.com>
2011-07-22 12:14:52 -0300

    improving petals cookbook

--- chef-repo/cookbooks/petals/metadata.rb
+++ chef-repo/cookbooks/petals/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "CCSL - Centro de Competencia de Software Livre - IME/USP"
+maintainer_email "ccsl@ime.usp.br"
+license          "Apache 2.0"
 description      "Installs/Configures petals"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+depends "java"
