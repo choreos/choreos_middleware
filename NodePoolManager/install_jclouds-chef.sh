ff9bb6a4054937c46b0458b9eaed110229d3064a
Felipe Pontes Guimarães <felps@felps-MacBookPro.(none)>
2012-02-09 13:33:32 -0200

    Added lines of code to remove the downloaded source code after compilation

--- NodePoolManager/install_jclouds-chef.sh
+++ NodePoolManager/install_jclouds-chef.sh
@@ -2,4 +2,7 @@ cd ../..
 git clone https://github.com/jclouds/jclouds-chef.git
 cd jclouds-chef
 mvn install
-cd ../choreos_middleware/NodePoolManager
+cd ..
+rm -R jclouds-chef
+rmdir jclouds-chef
+cd choreos_middleware/NodePoolManager
