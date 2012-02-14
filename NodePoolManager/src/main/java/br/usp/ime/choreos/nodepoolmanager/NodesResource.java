e6be85ee4aeb7bacb87b01c0a20e9d31dec37926
Felipe <felipepg@ime.usp.br>
2012-02-14 13:33:15 -0200

    Removed copilation warnings and reorganized some imports

--- NodePoolManager/src/main/java/br/usp/ime/choreos/nodepoolmanager/NodesResource.java
+++ NodePoolManager/src/main/java/br/usp/ime/choreos/nodepoolmanager/NodesResource.java
@@ -1,7 +1,6 @@
 package br.usp.ime.choreos.nodepoolmanager;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -9,14 +8,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
-
-import org.jclouds.compute.RunNodesException;
-
-import br.usp.ime.choreos.nodepoolmanager.cloudprovider.AWSCloudProvider;
-import br.usp.ime.choreos.nodepoolmanager.cloudprovider.CloudProvider;
 
 @Path("/nodes")
 public class NodesResource {
