f041acb328898074658bba6411979d6d7089ed69
Felipe <felipepg@ime.usp.br>
2012-02-18 00:41:06 -0200

    Edited layout in the Recipe default.rb attributes file

--- ServiceDeployer/src/main/resources/tomcat-service-deploy-template/attributes/default.rb
+++ ServiceDeployer/src/main/resources/tomcat-service-deploy-template/attributes/default.rb
@@ -9,15 +9,15 @@
 
 
 # Name of the deployed service 
-default['service']['$NAME']['WarFile']		= "$WARFILE"
-default['service']['$NAME']['URL']		= "$URL"
+default['service']['$NAME']['WarFile']	    = "$WARFILE"
+default['service']['$NAME']['URL']          = "$URL"
 
 # Set the default path to tomcat's webapps folder  
-default['service']['$NAME']['webappsPath']	= "$WEBAPPS"
+default['service']['$NAME']['webappsPath']  = "$WEBAPPS"
 
 # Set the default log file
-default['service']['$NAME']['logFile']		= "$LOGFILE"
+default['service']['$NAME']['logFile']      = "$LOGFILE"
 
 # Set the default log file
-default['service']['$NAME']['bashscript']	= "$BASHSCRIPT"
+default['service']['$NAME']['bashscript']   = "$BASHSCRIPT"
 
