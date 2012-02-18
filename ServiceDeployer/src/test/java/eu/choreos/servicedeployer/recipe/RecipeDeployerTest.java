6852a9dbf884521183c707cb6d8f6cf14d8abd5c
Felipe <felipepg@ime.usp.br>
2012-02-18 01:34:48 -0200

    Imported tests from StorageManager project
    Minor adaptations were required
    The registry test code was removed due to the different implementations

---
+++ ServiceDeployer/src/test/java/eu/choreos/servicedeployer/recipe/RecipeDeployerTest.java
@@ -0,0 +1,41 @@
+package eu.choreos.servicedeployer.recipe;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import eu.choreos.servicedeployer.NodePoolManagerHandler;
+import eu.choreos.storagefactory.utils.CommandLineInterfaceHelper;
+
+public class RecipeDeployerTest {
+
+	private static Recipe recipe;
+	private RecipeDeployer deployer = new RecipeDeployer(
+			new NodePoolManagerHandler());
+
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		recipe = new Recipe();
+		recipe.setName("storage123456789");
+		recipe.setFolder("./src/test/resources/chef/");
+		System.out
+				.println("deleting previous instances of the recipe. Errors are expected");
+		(new CommandLineInterfaceHelper())
+				.runLocalCommand("knife cookbook remove storage123456789 -y");
+	}
+
+	@Test
+	public void testDeployRecipe() {
+
+		deployer.deployRecipe(recipe);
+
+		String commandReturn = "";
+
+		commandReturn = (new CommandLineInterfaceHelper())
+				.runLocalCommand("knife cookbook list");
+
+		assertTrue("Did not find the uploaded recipe.",
+				commandReturn.contains("storage123456789"));
+	}
+}
---
+++ ServiceDeployer/src/test/java/eu/choreos/servicedeployer/registry/StorageNodeRegistryTest.java
@@ -0,0 +1,33 @@
+package eu.choreos.servicedeployer.registry;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.choreos.storagefactory.utils.CommandLineInterfaceHelper;
+
+public class StorageNodeRegistryTest {
+
+	protected DeployedServicesRegistry registry = new DeployedServicesRegistry();
+	protected static CommandLineInterfaceHelper cliHelper;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNodes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void shouldGetNodeFromUUIDForReal() {
+		fail("Not yet implemented");
+	}
+}
