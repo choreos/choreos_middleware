e52c049907a7d59208eca15719fefa7c3ded9ded
Felipe <felipepg@ime.usp.br>
2012-02-18 01:14:02 -0200

    Set static variables in attributes file

--- ServiceDeployer/src/main/resources/tomcat-service-deploy-template/attributes/default.rb
+++ ServiceDeployer/src/main/resources/tomcat-service-deploy-template/attributes/default.rb
@@ -13,11 +13,11 @@ default['service']['$NAME']['WarFile']	    = "$WARFILE"
 default['service']['$NAME']['URL']          = "$URL"
 
 # Set the default path to tomcat's webapps folder  
-default['service']['$NAME']['webappsPath']  = "$WEBAPPS"
+default['service']['$NAME']['webappsPath']  = "/var/lib/tomcat6/webapps"
+
+# Set the default log filewar_depl
+default['service']['$NAME']['logFile']      = "/dev/stdout"
 
 # Set the default log file
-default['service']['$NAME']['logFile']      = "$LOGFILE"
-
-# Set the default log file
-default['service']['$NAME']['bashscript']   = "$BASHSCRIPT"
+default['service']['$NAME']['bashscript']   = "war_deploy_script_service_$NAME.sh"
 
