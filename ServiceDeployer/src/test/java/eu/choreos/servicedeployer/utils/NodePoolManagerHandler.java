b7e0ebb429dde3497298a515b67d94b3191261b7
Felipe <felipepg@ime.usp.br>
2012-02-18 02:06:31 -0200

    Added a NodePoolManagerHandler to ServiceDeployer

---
+++ ServiceDeployer/src/test/java/eu/choreos/servicedeployer/utils/NodePoolManagerHandler.java
@@ -0,0 +1,26 @@
+package eu.choreos.servicedeployer.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NodePoolManagerHandler {
+
+	private static List<String> deployedRecipe = new ArrayList<String>();
+	
+	public String createNode(String recipe) {
+		deployedRecipe.add(recipe);
+		return "choreos-node";
+	}
+
+	public String getNode(String nodeId) {
+		return "choreos-node";
+	}
+
+	public List<String> getNodes() {
+		return deployedRecipe;
+	}
+
+	public void destroyNode(String id) {
+	}
+
+}
