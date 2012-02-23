7e0831f5305db34444abcaefebaec8ec36c83983
rafaeljpc <rafaeljpc@gmail.com>
2012-02-23 19:10:45 -0200

    Creating the server and doing some tests to learn how hadoop configuration is written on stdout

--- HadoopWS/src/main/br/usp/ime/ccsl/choreos/hadoop/HadoopWS.java
+++ HadoopWS/src/main/br/usp/ime/ccsl/choreos/hadoop/HadoopWS.java
@@ -1,4 +1,7 @@
-package br.usp.ime.ccsl.choreos;
+package br.usp.ime.ccsl.choreos.hadoop;
+
+import java.io.File;
+import java.io.PrintWriter;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,18 +11,23 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
+import org.apache.hadoop.conf.Configuration;
+
 
 @Path("/hadoop/")
 public class HadoopWS {
 
 
 	@GET
-	public Response getIntegradeApplication(@PathParam("application_id") String id,
-			@Context UriInfo uriInfo) {
+	public Response getIntegradeApplication(@Context UriInfo uriInfo) {
 
 		Response response = null;
 
 		try {
+			File hdConfPath = new File(System.getProperty("hadoop.home", System.getProperty("user.home")) + File.separator +
+										"conf");
+			Configuration conf = new Configuration(true);
+			conf.writeXml(System.out);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
