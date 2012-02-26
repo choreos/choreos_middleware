1c836cf28153155e47eec89a9429caf208dcff1a
alfonso777 <vphocco@gmail.com>
2012-02-26 12:57:58 -0300

    Correcting test and first interation with NodePoolManager

---
+++ ServiceDeployer/src/main/java/eu/choreos/servicedeployer/Configuration.java
@@ -0,0 +1,33 @@
+package eu.choreos.servicedeployer;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration {
+
+    private final Properties properties = new Properties();
+    private static String FILE_CONFIGURATION= "servicedeployer.properties";
+
+    private static Configuration INSTANCE = new Configuration();
+
+    private Properties getProperties() {
+        return properties;
+    }
+
+    public static String get(String key) {
+        return INSTANCE.getProperties().getProperty(key);
+    }
+
+    public static void set(String key, String value) {
+        INSTANCE.getProperties().setProperty(key, value);
+    }
+
+    private Configuration() {
+        try {
+            properties.load(ClassLoader.getSystemResourceAsStream(FILE_CONFIGURATION));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ ServiceDeployer/src/main/java/eu/choreos/servicedeployer/NodePoolManager.java
@@ -0,0 +1,60 @@
+package eu.choreos.servicedeployer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+
+import eu.choreos.servicedeployer.datamodel.Service;
+
+
+public class NodePoolManager implements NodePoolManagerHandler{
+
+	private static String HOST = "http://localhost:8082/";
+	protected static final WebClient client = WebClient.create(HOST);
+
+	public NodePoolManager(){
+		
+	}
+	
+	@Override
+	public Service createNode(String recipe, Service service) {
+		//TODO To integrate with NodePoolManager 
+//		client.path("nodes/configs");   	
+//		Config config = new Config();
+//    	config.setName(recipe);
+//        Response response = client.post(config);
+//
+//        return (String) response.getMetadata().get("Location").get(0);
+
+		return null;
+	}
+
+	@Override
+	public String getNode(String nodeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getNodes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroyNode(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initializeNode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
