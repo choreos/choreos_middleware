61f59b3a4ab01c9498abe5ec90c4f23f3ca254fd
Leonardo Leite <leonardofl87@gmail.com>
2012-02-29 11:26:06 -0300

    real client for NPM in ServiceDeployer

---
+++ ServiceDeployer/src/main/java/eu/choreos/servicedeployer/npm/NodePoolManagerClient.java
@@ -0,0 +1,29 @@
+package eu.choreos.servicedeployer.npm;
+
+
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+
+import eu.choreos.nodepoolmanager.datamodel.Config;
+
+
+public class NodePoolManagerClient implements NodePoolManager {
+
+	private static String HOST = "http://localhost:8080/";
+	protected static final WebClient client = WebClient.create(HOST);
+	
+	@Override
+	public String applyConfig(String configName) {
+
+		client.path("nodes/configs");   	
+		Config config = new Config();
+    	config.setName(configName);
+        Response response = client.post(config);
+
+        return (String) response.getMetadata().get("Location").get(0);
+	}
+    
+	
+
+}
