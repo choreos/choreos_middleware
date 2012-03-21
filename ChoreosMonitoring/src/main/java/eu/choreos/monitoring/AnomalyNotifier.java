bb6f5d5789f4f930ff011a8cd45fe43eb097e35a
Felipe <felipepg@ime.usp.br>
2012-03-21 17:13:04 -0300

    Created Notifier that evaluates the metrics map generated my GMondReader
    It implements methods that search for specified thresholds, currently
    hardcoded, and tells whether they were respected or surpassed.

---
+++ ChoreosMonitoring/src/main/java/eu/choreos/monitoring/AnomalyNotifier.java
@@ -0,0 +1,41 @@
+package eu.choreos.monitoring;
+
+import java.util.Map;
+
+public class AnomalyNotifier {
+
+	private GmondDataReader dataReader;
+	private Map<String, Gmetric> metricsMap;
+
+	public AnomalyNotifier(GmondDataReader dataReader) {
+		this.dataReader = dataReader;
+	}
+
+	public void setDataReader(GmondDataReader dataReader) {
+		this.dataReader = dataReader;
+	}
+
+	public boolean identifySurpassedUpperThreshold(String metricName,
+			double threshold) {
+		return (getMetricNumericalValue(metricName) > threshold);
+	}
+
+	public boolean identifySurpassedLowerThreshold(String metricName,
+			double threshold) {
+		return (getMetricNumericalValue(metricName) < threshold);
+	}
+
+	private double getMetricNumericalValue(String metricName) {
+		metricsMap = dataReader.getAllMetrics();
+
+		if (metricsMap == null) {
+			System.out.println("ERROR: Could not get metrics");
+			System.exit(1);
+		}
+		System.out.println(metricsMap.get(metricName).getValue());
+		System.out.println(Double.parseDouble(metricsMap.get(metricName)
+				.getValue()));
+		return Double.parseDouble(metricsMap.get(metricName).getValue());
+	}
+
+}
---
+++ ChoreosMonitoring/src/test/java/eu/choreos/monitoring/TestAnomalyNotifier.java
@@ -0,0 +1,80 @@
+package eu.choreos.monitoring;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestAnomalyNotifier {
+
+	private AnomalyNotifier notifier;
+	private GmondDataReader dataReader;
+	private HashMap<String, Gmetric> returnedMap;
+
+	@Before
+	public void setUp() throws Exception {
+		returnedMap = new HashMap<String, Gmetric>();
+		returnedMap.put("load_one", new Gmetric("load_one", "0.8"));
+		returnedMap.put("mem_total", new Gmetric("mem_total", "9876543"));
+	
+		dataReader = mock(GmondDataReader.class);
+		when(dataReader.getAllMetrics()).thenReturn(returnedMap);
+		
+		notifier = new AnomalyNotifier(dataReader);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void shouldIdentifyLoadAverageGreaterThanThreeInTheLastFiveMinutes() {
+		
+		returnedMap.put("load_five", new Gmetric("load_five", "3.8"));
+		
+		boolean evaluation = notifier.identifySurpassedUpperThreshold("load_five", 3);
+		
+		assertTrue(evaluation);
+		
+	}
+	
+	@Test
+	public void shouldNotIdentifyLoadAverageGreaterThanThreeInTheLastFiveMinutes() {
+		
+		returnedMap.put("load_five", new Gmetric("load_five", "1.8"));
+		
+		boolean evaluation = notifier.identifySurpassedUpperThreshold("load_five", 3);
+		
+		assertFalse("Evaluated threshold should be false", evaluation);
+		
+	}
+	
+	@Test
+	public void shouldIdentifyFreeMemoryLessThan64Mb() {
+		returnedMap.put("mem_free", new Gmetric("mem_free", "16000"));
+		
+		boolean evaluation = notifier.identifySurpassedLowerThreshold("mem_free", 64000);
+		
+		assertTrue("This host has only 16MB of free memory. Should not have been triggered.", evaluation);
+		
+	}
+
+	@Test
+	public void shouldNotIdentifyFreeMemoryLessThan64Mb() {
+		returnedMap.put("mem_free", new Gmetric("mem_free", "176000"));
+		
+		boolean evaluation = notifier.identifySurpassedLowerThreshold("mem_free", 64000);
+		
+		assertFalse("This host has 176MB of free memory. Should not have been triggered.", evaluation);
+		
+	}
+
+
+}
