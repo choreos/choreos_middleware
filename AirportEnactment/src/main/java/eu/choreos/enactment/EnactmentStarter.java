5f5b07ff9a39a24a802809ffdb0e01d07497e24f
Leonardo Leite <leonardofl87@gmail.com>
2012-04-24 22:18:57 -0300

    Concurrent deployments in AirportEnactment

--- AirportEnactment/src/main/java/eu/choreos/enactment/EnactmentStarter.java
+++ AirportEnactment/src/main/java/eu/choreos/enactment/EnactmentStarter.java
@@ -12,19 +12,69 @@ public class EnactmentStarter {
 	private Enacter consumeEnacter = EnacterFactory.getSoapConsumeEnacter();
 	private Enacter provideEnacter = EnacterFactory.getSoapProvideEnacter();
 	
+	private Set<Service> powss, provides, consumes;
+	
 	public void enact() {
 		
 		System.out.println("Starting airport enactment...");
-		Set<Service> powss = powsEnacter.enact();
-		Set<Service> provides = provideEnacter.enact();
-		Set<Service> consumes = consumeEnacter.enact();
-		cdEnacter.enact();
 
-		System.out.println("Passing context");
-		ChorContextCaster caster = new ChorContextCaster();
-		caster.cast(powss, provides, consumes);
+		this.deployArtifacts();
+		this.passContext();
+	}
+
+	private void deployArtifacts() {
+
+		// each thread enacts a different group of artifacts
+		Thread[] trds = new Thread[4];
+		
+		trds[0] = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				powss = powsEnacter.enact();				
+			}
+		});
+		trds[0].start();
+
+		trds[1] = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				consumes = consumeEnacter.enact();				
+			}
+		});
+		trds[1].start();
+		
+		trds[2] = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				provides = provideEnacter.enact();				
+			}
+		});
+		trds[2].start();
+		
+		trds[3] = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				cdEnacter.enact();			
+			}
+		});
+		trds[3].start();
+		
+		// wait for all threads finish
+		for (Thread t: trds) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
+	private void passContext() {
+		
+		System.out.println("Passing context");
+		ChorContextCaster caster = new ChorContextCaster();
+		caster.cast(powss, provides, consumes);		
+	}
 	
 	public static void main(String[] args) {
 
