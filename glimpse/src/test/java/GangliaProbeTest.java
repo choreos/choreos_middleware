55adb0307ec1eb1fba10f6e7b72540f829c21a62
Thiago Furtado <thiagofurtado17@gmail.com>
2012-04-25 18:17:24 -0300

    Moved tests for hostname.sh to GangliaProbeTest.java

---
+++ glimpse/src/test/java/GangliaProbeTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.choreos.monitoring.glimpse.probe.GangliaProbe;
+import eu.choreos.monitoring.utils.ShellHandler;
+
+import it.cnr.isti.labse.glimpse.utils.Manager;
+
+public class GangliaProbeTest {
+	
+	GangliaProbe gangliaProbe;
+	
+	String hostname = ShellHandler.runLocalCommand("/bin/bash "+ "/tmp/hostname.sh").replace("\n", "");
+
+	@Before
+	public void setUp() throws Exception {
+		String javaNamingProviderUrl = "tcp://dsbchoreos.petalslink.org:61616";
+		Properties createProbeSettingsPropertiesObject = Manager.createProbeSettingsPropertiesObject(
+				"org.apache.activemq.jndi.ActiveMQInitialContextFactory",
+				javaNamingProviderUrl, 
+				"system", 
+				"manager", 
+				"GangliaFactory", 
+				"jms.probeTopic", 
+				true,
+				"probeName", 
+				"probeTopic");
+		
+		gangliaProbe = new GangliaProbe(createProbeSettingsPropertiesObject);
+	}
+
+	@Test
+	public void testGetScriptCommand() {
+		assertEquals("/bin/bash " + "/tmp/hostname.sh", gangliaProbe.getScriptCommand());
+	}
+	
+	@Test
+	public void shouldGetHostname() throws Exception {
+		assertEquals(hostname, gangliaProbe.getHostName());
+	}
+
+}
