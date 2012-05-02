9e58de0315974a9ed52d0debf2fb043fe7a74d0a
Leonardo Leite <leonardofl87@gmail.com>
2012-05-02 17:45:07 -0300

    NodeSelectorFactory

---
+++ NodePoolManager/src/main/java/eu/choreos/nodepoolmanager/selector/NodeSelectorFactory.java
@@ -0,0 +1,55 @@
+package eu.choreos.nodepoolmanager.selector;
+
+import eu.choreos.nodepoolmanager.Configuration;
+import eu.choreos.nodepoolmanager.cloudprovider.CloudProvider;
+
+public class NodeSelectorFactory {
+
+	public enum NodeSelectorType {
+		VERY_SIMPLE, ROUND_ROBIN
+	};
+
+	public static NodeSelector getNodeSelectorInstance(
+			CloudProvider cloudProvider) {
+
+		String selector = Configuration.get("NODE_SELECTOR");
+		NodeSelectorType type = null;
+		try {
+			type = NodeSelectorType.valueOf(selector);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalStateException(
+					"Invalid NODE_SELECTOR in properties file");
+		}
+		return getNodeSelectorInstance(cloudProvider, type);
+	}
+
+	private static NodeSelector getNodeSelectorInstance(
+			CloudProvider cloudProvider, NodeSelectorType nodeSelectorType) {
+
+		switch (nodeSelectorType) {
+
+		case VERY_SIMPLE:
+			return new VerySimpleSelector(cloudProvider);
+
+		case ROUND_ROBIN:
+			String nodesQuantityStr = null;
+			nodesQuantityStr = Configuration.get("NODES_QUANTITY");
+			if (nodesQuantityStr == null) {
+				throw new IllegalStateException(
+						"Properties file must declare NODES_QUANTITY");
+			}
+			int nodesQuantity;
+			try {
+				nodesQuantity = Integer.parseInt(nodesQuantityStr);
+			} catch (NumberFormatException e) {
+				throw new IllegalStateException(
+						"Invalid NODES_QUANTITY in properties file");
+			}
+			return new SimpleRoundRobinSelector(cloudProvider, nodesQuantity);
+
+		default:
+			throw new IllegalStateException("Could not choose NodeSelector");
+		}
+	}
+
+}
