964869d374d42315adf7e831be4f7d89300f8c68
Nelson Lago <lago@ime.usp.br>
2012-05-05 03:16:49 -0300

    Fix version number for ganglia recipe

--- chef-repo/cookbooks/ganglia/metadata.rb
+++ chef-repo/cookbooks/ganglia/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures ganglia - modified to work in the CHOReOS context by <lago@ime.usp.br>"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
+version          "0.1.3"
 
 %w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
