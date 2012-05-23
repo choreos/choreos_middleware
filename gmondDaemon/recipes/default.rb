58ef768c737cf4d7b4dd3a73d6b5cf86df26efcc
Felipe <felipepg@ime.usp.br>
2012-05-23 15:56:46 -0300

    Fixed gmondDaemon recipe

--- gmondDaemon/recipes/default.rb
+++ gmondDaemon/recipes/default.rb
@@ -11,14 +11,4 @@ include_recipe "ganglia"
 
 execute "set_hostname" do
   command "gmetric --name hostname --value #{node['hostname']} --type string"
-  notifies :create, "ruby_block[set_hostname_once]", :immediately
-  not_if { node.attribute?("hostname_was_set") }
-end
- 
-ruby_block "set_hostname_once" do
-  block do
-    node.set['hostname_was_set'] = true
-    node.save
-  end
-  action :nothing
 end
