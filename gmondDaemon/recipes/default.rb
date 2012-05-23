7121966a0d7763f800530fe128c0a22c2a3b44e5
Thiago Furtado <thiagofurtado17@gmail.com>
2012-05-23 15:39:25 -0300

    created gmondDaemon chef recipe

---
+++ gmondDaemon/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: gmondDaemon
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "ganglia"
+
+execute "set_hostname" do
+  command "gmetric --name hostname --value #{node['hostname']} --type string"
+  notifies :create, "ruby_block[some_command_run_flag]", :immediately
+  not_if { node.attribute?("hostname_was_set") }
+end
+ 
+ruby_block "set_hostname_once" do
+  block do
+    node.set['hostname_was_set'] = true
+    node.save
+  end
+  action :nothing
+end
