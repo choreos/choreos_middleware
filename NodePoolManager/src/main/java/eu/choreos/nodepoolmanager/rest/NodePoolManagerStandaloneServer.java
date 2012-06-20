625b6bde769803e2ad0cabdd094ac54501c37afd
Leonardo Leite <leonardofl87@gmail.com>
2012-06-20 19:41:43 -0300

    Log configurator extracted to new class

--- NodePoolManager/src/main/java/eu/choreos/nodepoolmanager/rest/NodePoolManagerStandaloneServer.java
+++ NodePoolManager/src/main/java/eu/choreos/nodepoolmanager/rest/NodePoolManagerStandaloneServer.java
@@ -1,20 +1,15 @@
 package eu.choreos.nodepoolmanager.rest;
 
-import java.io.IOException;
-import java.util.Properties;
-
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 import eu.choreos.nodepoolmanager.Configuration;
+import eu.choreos.nodepoolmanager.utils.LogConfigurator;
 
 
 
 public class NodePoolManagerStandaloneServer implements Runnable {
 
-	private static final String LOG_CONFIG_FILE = "log.properties";
 	private static Logger logger;
 	
 	public static String URL;
@@ -25,19 +20,6 @@ public class NodePoolManagerStandaloneServer implements Runnable {
     	URL = "http://localhost:" + port + "/nodepoolmanager/";
     }
 
-    private static void configureLog() {
-    	
-    	Properties logProperties = new Properties();
-    	try {
-			logProperties.load(ClassLoader.getSystemResourceAsStream(LOG_CONFIG_FILE));
-			PropertyConfigurator.configure(logProperties); 
-		} catch (IOException e) {
-			System.out.println("Could not load log.properties. Let's use basic log.");
-			BasicConfigurator.configure();
-		} 
-    }
-
-    
     public static void startNodePoolManager() throws InterruptedException {
         Runnable npmServer = new NodePoolManagerStandaloneServer();
 		
@@ -74,7 +56,7 @@ public class NodePoolManagerStandaloneServer implements Runnable {
 
     public static void main(String[] args) throws InterruptedException {
     	
-    	configureLog();
+    	LogConfigurator.configLog();
         logger = Logger.getLogger(NodePoolManagerStandaloneServer.class);
         
         NodePoolManagerStandaloneServer.startNodePoolManager();
---
+++ NodePoolManager/src/main/java/eu/choreos/nodepoolmanager/utils/LogConfigurator.java
@@ -0,0 +1,24 @@
+package eu.choreos.nodepoolmanager.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+public class LogConfigurator {
+
+	private static final String LOG_CONFIG_FILE = "log.properties";
+	
+	public static void configLog() {
+	    	
+    	Properties logProperties = new Properties();
+    	try {
+			logProperties.load(ClassLoader.getSystemResourceAsStream(LOG_CONFIG_FILE));
+			PropertyConfigurator.configure(logProperties); 
+		} catch (IOException e) {
+			System.out.println("Could not load log.properties. Let's use basic log.");
+			BasicConfigurator.configure();
+		} 
+	}
+}
