1dac0e46c58d05084be84cd1ae67a6d17a936ad3
Thiago Furtado <thiagofurtado17@gmail.com>
2012-07-05 03:42:17 -0300

    add tests to DoubleThreshold

--- Monitoring/ThresholdEvaluationDaemon/src/test/java/eu/choreos/monitoring/platform/daemon/ThresholdTest.java
+++ Monitoring/ThresholdEvaluationDaemon/src/test/java/eu/choreos/monitoring/platform/daemon/ThresholdTest.java
@@ -1,38 +1,43 @@
 package eu.choreos.monitoring.platform.daemon;
 
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import eu.choreos.platform.utils.CommandRuntimeException;
 
-public class ThresholdTest extends TestCase  {
+public class ThresholdTest {
 
 	SingleThreshold threshold;
+	DoubleThreshold dthreshold;
 
 	@Before
 	public void setUp() throws CommandRuntimeException {
 		threshold = new SingleThreshold("Test", SingleThreshold.MAX, 3);
+		dthreshold = new DoubleThreshold(
+				"Test2", DoubleThreshold.BETWEEN, 1.0, 2.0);
 	}
 
 	@Test
 	public void upperBoundarySurpassed() {
 		assertTrue(threshold.wasSurpassed(4.0));
+		assertTrue(dthreshold.wasSurpassed(3.0));
 	}
 
 	@Test
 	public void upperBoundaryNotSurpassed() {
 		assertFalse(threshold.wasSurpassed(2.0));
+		assertFalse(dthreshold.wasSurpassed(1.5));
 	}
 
 	@Test
 	public void lowerBoundarySurpassed() {
 		threshold = new SingleThreshold("Test", SingleThreshold.MIN, 3);
 		assertTrue(threshold.wasSurpassed(2.0));
+		assertTrue(dthreshold.wasSurpassed(0.5));
 	}
 
 	@Test
