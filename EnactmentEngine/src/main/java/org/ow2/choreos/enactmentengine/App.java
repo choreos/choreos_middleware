5bf618d6c587ddc56cd3a9aec8bc35c1149656d1
Leonardo Leite <leonardofl87@gmail.com>
2012-07-18 17:21:29 -0300

    Enactment Engine project structure

---
+++ EnactmentEngine/src/main/java/org/ow2/choreos/enactmentengine/App.java
@@ -0,0 +1,13 @@
+package org.ow2.choreos.enactmentengine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
