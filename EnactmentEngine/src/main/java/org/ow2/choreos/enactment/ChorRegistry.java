f784e5f5baf94e3a344ef78d229ddd18a6cfc450
Leonardo Leite <leonardofl87@gmail.com>
2012-08-01 16:00:57 -0300

    ChorRegistry

---
+++ EnactmentEngine/src/main/java/org/ow2/choreos/enactment/ChorRegistry.java
@@ -0,0 +1,63 @@
+package org.ow2.choreos.enactment;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.ow2.choreos.enactment.datamodel.ChorService;
+import org.ow2.choreos.enactment.datamodel.Choreography;
+
+/**
+ * Stores choreography descriptions.
+ * 
+ * TODO: In the future, we must use a database instead a static map.
+ * 
+ * @author leonardo
+ *
+ */
+public class ChorRegistry {
+	
+	private static ChorRegistry instance;
+	
+	// map key is the chor id
+	private Map<String, Choreography> chors = new HashMap<String, Choreography>(); 
+	private AtomicInteger counter = new AtomicInteger();
+	
+	private ChorRegistry() {
+		
+	}
+	
+	public static ChorRegistry getInstance() {
+		
+		if (instance == null)
+			instance = new ChorRegistry();
+		return instance;
+	}
+	
+	/**
+	 * Creates a new choreography entry
+	 * @return the choreography id
+	 */
+	public String newChor() {
+		
+		String id = Integer.toString(counter.incrementAndGet());
+		Choreography chor = new Choreography();
+		chors.put(id, chor);
+		
+		return id;
+	}
+	
+	/**
+	 * Associates a service to a choreography
+	 * @param service the service specification
+	 * @param chorId the choreography id
+	 */
+	public void addService(ChorService service, String chorId) {
+		
+		Choreography chor = chors.get(chorId);
+		if (chorId == null)
+			throw new IllegalArgumentException();
+		chor.getServices().add(service);
+	}
+
+}
