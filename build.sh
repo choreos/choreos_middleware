699da1778cfbf2c4ee35edb1637f6b6cd07d3b65
Leonardo Leite <leonardofl87@gmail.com>
2012-08-02 17:08:54 -0300

    Really solving Rehearsal x CXF dependence conflict

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 mvn clean javadoc:jar source:jar install -Dmaven.test.skip=true
 mvn eclipse:clean eclipse:eclipse
-cp $HOME/.m2/repository/eu/choreos/rehearsal/0.13/rehearsal-0.13.jar EnactmentEngine/lib/
