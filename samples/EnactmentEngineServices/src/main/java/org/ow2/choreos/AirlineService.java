9596b564142c8e49cb95e46555401f86a044b0fe
Leonardo Leite <leonardofl87@gmail.com>
2012-08-27 10:24:16 -0300

    EE sample services with simple logger

--- samples/EnactmentEngineServices/src/main/java/org/ow2/choreos/AirlineService.java
+++ samples/EnactmentEngineServices/src/main/java/org/ow2/choreos/AirlineService.java
@@ -3,14 +3,21 @@ package org.ow2.choreos;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.ow2.choreos.log.SimpleLogger;
+import org.ow2.choreos.log.SimpleLoggerImpl;
+
 @WebService
 public class AirlineService implements Airline {
+
+	private final SimpleLogger logger = new SimpleLoggerImpl("airline.log");
 
 	@WebMethod
 	@Override
 	public String buyFlight() {
 
-		return "33";
+		String result = "33";
+		logger.info("Request to buy flight; response: " + result);
+		return result;
 	}
 
 }
--- samples/EnactmentEngineServices/src/main/java/org/ow2/choreos/TravelAgencyService.java
+++ samples/EnactmentEngineServices/src/main/java/org/ow2/choreos/TravelAgencyService.java
@@ -6,6 +6,8 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import org.apache.xmlbeans.XmlException;
+import org.ow2.choreos.log.SimpleLogger;
+import org.ow2.choreos.log.SimpleLoggerImpl;
 
 import eu.choreos.vv.clientgenerator.Item;
 import eu.choreos.vv.clientgenerator.WSClient;
@@ -20,27 +22,36 @@ public class TravelAgencyService implements TravelAgency {
 	
 	private static String airlineWSDL;
 	
+	private final SimpleLogger logger = new SimpleLoggerImpl("travel_agency.log");
+	
 	@WebMethod
 	@Override
 	public String buyTrip() {
 
 		WSClient client = getAirlineClient();
 		
-		if (client == null)
+		if (client == null) {
+			logger.error(ERROR_MESSAGE + " (airline client is null)");
 			return ERROR_MESSAGE;
+		}
 		else {
 			try {
 			
 				Item response = client.request("buyFlight");
 				String flightTicketNumber = response.getChild("return").getContent();
 				String hotelReservationNumber = "22";
-				return flightTicketNumber + "--" + hotelReservationNumber ;
+				String result = flightTicketNumber + "--" + hotelReservationNumber;
+				logger.info("request to buy trip; response: " + result);
+				return  result;
 
 			} catch (InvalidOperationNameException e) {
+				logger.error("", e);
 				return ERROR_MESSAGE;
 			} catch (FrameworkException e) {
+				logger.error("", e);
 				return ERROR_MESSAGE;
 			} catch (NoSuchFieldException e) {
+				logger.error("", e);
 				return ERROR_MESSAGE;
 			}
 		}
@@ -55,12 +66,16 @@ public class TravelAgencyService implements TravelAgency {
 		try {
 			client = new WSClient(airlineWSDL);
 		} catch (WSDLException e) {
+			logger.error("get client failed", e);
 			return null;
 		} catch (XmlException e) {
+			logger.error("get client failed", e);
 			return null;
 		} catch (IOException e) {
+			logger.error("get client failed", e);
 			return null;
 		} catch (FrameworkException e) {
+			logger.error("get client failed", e);
 			return null;
 		}
 		
@@ -71,8 +86,12 @@ public class TravelAgencyService implements TravelAgency {
 	@Override
 	public void setInvocationAddress(String role, String endpoint) {
 
-		if (role.equals("airline"))
+		if (role.equals("airline")) {
+			logger.info("Endpoint to airline: " + endpoint);
 			airlineWSDL = endpoint + "?wsdl";
+		} else {
+			logger.warn("Invalid role (" + role + ") in setInvocationAddress");
+		}
 	}
 
 }
