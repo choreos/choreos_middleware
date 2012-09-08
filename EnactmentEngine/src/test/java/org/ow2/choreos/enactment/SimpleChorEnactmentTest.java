3a7bc72d8ba26f71762dbc65e7d962187e3b4b23
Leonardo Leite <leonardofl87@gmail.com>
2012-09-08 18:09:43 -0300

    code to write XML examples

---
+++ EnactmentEngine/src/test/java/org/ow2/choreos/enactment/SimpleChorEnactmentTest.java
@@ -0,0 +1,63 @@
+package org.ow2.choreos.enactment;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.ow2.choreos.enactment.datamodel.ChorSpec;
+import org.ow2.choreos.enactment.datamodel.Choreography;
+import org.ow2.choreos.npm.rest.NPMServer;
+import org.ow2.choreos.servicedeployer.datamodel.Service;
+import org.ow2.choreos.utils.LogConfigurator;
+
+import eu.choreos.vv.clientgenerator.Item;
+import eu.choreos.vv.clientgenerator.WSClient;
+
+/**
+ * This test will enact a choreography with two services,
+ * with a service depending on the other.
+ * 
+ * Before the test, start the NPMServer and the ServiceDeployerServer
+ *
+ * @author leonardo
+ *
+ */
+public class SimpleChorEnactmentTest {
+
+	private ChorSpec chorSpec;
+	
+	@BeforeClass
+	public static void startServers() {
+		LogConfigurator.configLog();
+	}
+	
+	@AfterClass
+	public static void shutDownServers() {
+		NPMServer.stop();
+	}
+	
+	@Before
+	public void setUp() {
+		
+		ModelsForTest models = new ModelsForTest();
+		chorSpec = models.getChorSpec(); 
+	}
+	
+	@Test
+	public void shouldEnactChoreography() throws Exception {
+		
+		EnactmentEngine ee = new EnactEngImpl();
+		String chorId = ee.createChoreography(chorSpec);
+		Choreography chor = ee.enact(chorId);
+
+		Service travel = chor.getDeployedServiceByName(ModelsForTest.TRAVEL_AGENCY);
+		WSClient client = new WSClient(travel.getUri() + "?wsdl");
+		Item response = client.request("buyTrip");
+		String codes = response.getChild("return").getContent();
+		
+		assertEquals("33--22", codes);
+	}
+
+}
