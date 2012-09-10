8e3134641715bd70c08c20ed520dc4fc3dc95cb4
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2012-09-10 15:35:25 -0300

    Added OpenStack support.

---
+++ ServiceDeployer/src/main/java/org/ow2/choreos/npm/cloudprovider/OpenStackKeystoneCloudProvider.java
@@ -0,0 +1,218 @@
+package org.ow2.choreos.npm.cloudprovider;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.jclouds.Constants;
+import org.jclouds.ContextBuilder;
+import org.jclouds.compute.ComputeService;
+import org.jclouds.compute.ComputeServiceContext;
+import org.jclouds.compute.RunNodesException;
+import org.jclouds.compute.domain.ComputeMetadata;
+import org.jclouds.compute.domain.Hardware;
+import org.jclouds.compute.domain.Image;
+import org.jclouds.compute.domain.NodeMetadata;
+import org.jclouds.compute.domain.Template;
+import org.jclouds.compute.domain.TemplateBuilder;
+import org.ow2.choreos.npm.NodeNotFoundException;
+import org.ow2.choreos.npm.datamodel.Node;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.inject.Module;
+
+public class OpenStackKeystoneCloudProvider implements CloudProvider {
+
+    public String getproviderName() {
+        return "";
+    }
+
+    private static String OP_AUTHURL = "http://198.55.33.14:5000/v2.0";
+    private static String OP_TENANT = "Choreos";
+    private static String OP_USER = "daniel";
+    private static String OP_PASS = "danielpass";
+
+    private ComputeService getClient(String imageId) {
+        System.out.println(">OpenStack: Obtain Client.");
+
+        String provider = "openstack-nova";
+        String identity = OP_TENANT + ":" + OP_USER;
+        String credential = OP_PASS;
+
+        Properties properties = new Properties();
+        properties.setProperty(Constants.PROPERTY_ENDPOINT, OP_AUTHURL);
+
+        // example of injecting a ssh implementation
+        Iterable<Module> modules = ImmutableSet.<Module> of();
+
+        ContextBuilder builder = ContextBuilder.newBuilder(provider)
+                .credentials(identity, credential).modules(modules).overrides(properties);
+
+        System.out.printf(">OpenStack: Initializing Client With Data: \n	> %s%n",
+                builder.getApiMetadata());
+
+        ComputeService srv = builder.buildView(ComputeServiceContext.class).getComputeService();
+
+        System.out.println(">OpenStack: Client obtained successfully.");
+
+        return srv;
+    }
+
+    @Override
+    public Node createNode(Node node) throws RunNodesException {
+        System.out.println(">OpenStack: Create new Node.");
+
+        ComputeService client = getClient("");
+        Set<? extends NodeMetadata> createdNodes;
+
+        try {
+            createdNodes = client.createNodesInGroup("default", 1,
+                    getTemplate(client, getImages().get(0).getId()));
+        } catch (org.jclouds.rest.AuthorizationException e) {
+            System.out
+                    .println(">OpenStack: Authorization failed. Provided user doesn't have authorization to create a new node.");
+            throw e;
+        }
+
+        NodeMetadata cloudNode = Iterables.get(createdNodes, 0);
+
+        setNodeProperties(node, cloudNode);
+        client.getContext().close();
+
+        System.out.println(">OpenStack: Node created successfully.");
+        return node;
+    }
+
+    @Override
+    public Node getNode(String nodeId) throws NodeNotFoundException {
+        ComputeService client = getClient("");
+
+        Node node = new Node();
+
+        try {
+            NodeMetadata cloudNode = client.getNodeMetadata(nodeId);
+            setNodeProperties(node, cloudNode);
+        } catch (Exception e) {
+            throw new NodeNotFoundException(nodeId);
+        }
+
+        return node;
+    }
+
+    @Override
+    public List<Node> getNodes() {
+        System.out.println(">OpenStack: List Nodes.");
+        List<Node> nodeList = new ArrayList<Node>();
+        Node node;
+
+        ComputeService client = getClient("");
+        Set<? extends ComputeMetadata> cloudNodes = client.listNodes();
+        for (ComputeMetadata computeMetadata : cloudNodes) {
+            NodeMetadata cloudNode = client.getNodeMetadata(computeMetadata.getId());
+            node = new Node();
+
+            setNodeProperties(node, cloudNode);
+            if (node.getState() != 1) {
+                nodeList.add(node);
+            }
+        }
+
+        client.getContext().close();
+
+        System.out.println(">OpenStack: Node List obtained successfully.");
+
+        return nodeList;
+    }
+
+    public List<Image> getImages() {
+        System.out.println(">OpenStack: List Images.");
+        ComputeService client = getClient("");
+        Set<? extends Image> images = client.listImages();
+
+        List<Image> imageList = new ArrayList<Image>();
+
+        for (Image image : images) {
+            imageList.add(image);
+        }
+
+        System.out.println(">OpenStack: Image List obtained successfully.");
+
+        return imageList;
+
+    }
+
+    public List<Hardware> getHardwareProfiles() {
+        System.out.println(">OpenStack: List Hardware Profiles.");
+        ComputeService client = getClient("");
+        Set<? extends Hardware> profiles = client.listHardwareProfiles();
+
+        List<Hardware> hardwareList = new ArrayList<Hardware>();
+
+        for (Hardware profile : profiles) {
+            hardwareList.add(profile);
+        }
+
+        System.out.println(">OpenStack: Hardware Profiles obtained successfully.");
+
+        return hardwareList;
+
+    }
+
+    @Override
+    public void destroyNode(String id) {
+        System.out.println(">OpenStack: Destroy Node.");
+        ComputeService client = getClient("");
+        client.destroyNode(id);
+        client.getContext().close();
+        System.out.println(">OpenStack: Node destroyed successfully.");
+    }
+
+    @Override
+    public Node createOrUseExistingNode(Node node) throws RunNodesException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private Template getTemplate(ComputeService client, String imageId) {
+
+        if (imageId.isEmpty()) {
+            imageId = getImages().get(0).getId();
+        }
+
+        String hardwareId = getHardwareProfiles().get(0).getId();
+
+        System.out.println("	Creating Template with:");
+        System.out.println("	Image ID: " + imageId);
+        System.out.println("	Hardware ID: " + hardwareId);
+
+        TemplateBuilder builder = client.templateBuilder().imageId(imageId);
+        builder.hardwareId(hardwareId);
+        System.out.println("	Building Template...");
+        Template template = builder.build();
+        System.out.println("	Template built successfully!");
+        // TemplateOptions options = template.getOptions();
+        return template;
+    }
+
+    private void setNodeProperties(Node node, NodeMetadata cloudNode) {
+        setNodeIp(node, cloudNode);
+        node.setHostname(cloudNode.getName());
+        node.setSo(cloudNode.getOperatingSystem().getName());
+        node.setId(cloudNode.getId());
+        node.setImage(cloudNode.getImageId());
+        node.setState(cloudNode.getState().ordinal());
+        node.setUser("ubuntu");
+    }
+
+    private void setNodeIp(Node node, NodeMetadata cloudNode) {
+        Iterator<String> publicAddresses = cloudNode.getPrivateAddresses().iterator();
+
+        if (publicAddresses != null && publicAddresses.hasNext()) {
+            node.setIp(publicAddresses.next());
+        }
+    }
+
+}
---
+++ ServiceDeployer/src/test/java/org/ow2/choreos/npm/cloudprovider/OpenStackCloudProviderTest.java
@@ -0,0 +1,109 @@
+package org.ow2.choreos.npm.cloudprovider;
+
+import java.util.List;
+
+import org.jclouds.compute.RunNodesException;
+import org.jclouds.compute.domain.Hardware;
+import org.jclouds.compute.domain.Image;
+import org.junit.Before;
+import org.junit.Test;
+import org.ow2.choreos.npm.BaseTest;
+import org.ow2.choreos.npm.datamodel.Node;
+
+public class OpenStackCloudProviderTest extends BaseTest {
+
+    private final static OpenStackKeystoneCloudProvider infra = new OpenStackKeystoneCloudProvider();
+
+    // private Node node = new Node();
+
+    @Before
+    public void SetUp() throws RunNodesException {
+        // node.
+        System.out.println("Began Pre-OS Testing...");
+
+    }
+
+    @Test
+    public void shouldCreateNodeFromPool() throws Exception {
+        System.out.println(">>>> Starting Openstack Tests.");
+
+        boolean bTestNewNode = true;
+        boolean bTestNodesList = true;
+        boolean bTestImagesList = true;
+        boolean bTestHWList = true;
+
+        System.out.println(">>>> Will Test:");
+        if (bTestNewNode) {
+            System.out.println("	> New Node Creation");
+        }
+        if (bTestNodesList) {
+            System.out.println("	> Node List");
+        }
+        if (bTestImagesList) {
+            System.out.println("	> Image List");
+        }
+        if (bTestHWList) {
+            System.out.println("	> Hardware Profile List");
+        }
+
+        System.out.println("");
+
+        if (bTestNewNode) {
+            // Create new Node
+            System.out.println("TEST: Create New Node from OpenStack\n");
+            Node nodeNew = new Node();
+            infra.createNode(nodeNew);
+        }
+
+        if (bTestNodesList) {
+            // List nodes
+            System.out.println("TEST: List Nodes from OpenStack\n");
+            List<Node> nodes = infra.getNodes();
+
+            System.out.println("Node Count:" + nodes.size());
+
+            for (Node node : nodes) {
+                System.out.println("ID: " + node.getId());
+                System.out.println("SO: " + node.getSo());
+                System.out.println("Hostname: " + node.getHostname());
+                System.out.println("Image: " + node.getImage());
+                System.out.println("IP: " + node.getIp());
+                System.out.println("User: " + node.getUser());
+                System.out.println("--------------");
+            }
+        }
+
+        if (bTestImagesList) {
+            // Image List Test
+            System.out.println("TEST: List Images from OpenStack\n");
+            List<Image> images = infra.getImages();
+
+            System.out.println("Image Count: " + images.size());
+
+            for (Image image : images) {
+                System.out.println("Name: " + image.getName());
+                System.out.println("ID: " + image.getId());
+                System.out.println("OS: " + image.getOperatingSystem());
+                System.out.println("Location: " + image.getLocation());
+                System.out.println("--------------");
+            }
+        }
+
+        if (bTestHWList) {
+            // Hardware Profiles Test
+            System.out.println("TEST: List Hardware Profiles from OpenStack\n");
+            List<Hardware> hardwares = infra.getHardwareProfiles();
+
+            System.out.println("Hardware Count: " + hardwares.size());
+
+            for (Hardware hardware : hardwares) {
+                System.out.println("Name: " + hardware.getName());
+                System.out.println("ID: " + hardware.getId());
+                System.out.println("--------------");
+            }
+        }
+
+        System.out.println(">>>> Completed Openstack Tests.");
+    }
+
+}
