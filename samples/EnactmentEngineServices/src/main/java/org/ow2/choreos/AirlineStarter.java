e7258bd8b61717ba4ca21ea594f3c042ee33c533
Leonardo Leite <leonardofl87@gmail.com>
2012-09-22 11:54:13 -0300

    Rehearsal removed from EnactmentEngineServices
    Using javax classes instead Rehearsal decreased the JAR size from 24Mb to 220kb.
    Now I hope our tests will be much faster!
    (these JARs are used on EE tests)

--- samples/EnactmentEngineServices/src/main/java/org/ow2/choreos/AirlineStarter.java
+++ samples/EnactmentEngineServices/src/main/java/org/ow2/choreos/AirlineStarter.java
@@ -4,11 +4,18 @@ import javax.xml.ws.Endpoint;
 
 public class AirlineStarter {
 
-	public static void main(String[] args) {
+	public static final String SERVICE_ADDRESS = "http://0.0.0.0:1234/airline";
+	private static Endpoint endpoint;
+	
+	public static void start() {
 
 		Airline service = new AirlineService();
-		Endpoint endpoint = Endpoint.create(service);
-		endpoint.publish("http://0.0.0.0:1234/airline");
+		endpoint = Endpoint.create(service);
+		endpoint.publish(SERVICE_ADDRESS);
 	}
 
+	public static void main(String[] args) {
+		
+		start();
+	}
 }
