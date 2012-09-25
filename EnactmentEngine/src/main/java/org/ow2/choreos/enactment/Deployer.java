ced851a18001c1c43cd1f048451c512be9ef205f
Leonardo Leite <leonardofl87@gmail.com>
2012-09-25 14:34:56 -0300

    Concurrent invocations from EE to upgrade node

--- EnactmentEngine/src/main/java/org/ow2/choreos/enactment/Deployer.java
+++ EnactmentEngine/src/main/java/org/ow2/choreos/enactment/Deployer.java
@@ -1,6 +1,8 @@
 package org.ow2.choreos.enactment;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -21,36 +23,90 @@ public class Deployer {
 
 	private Logger logger = Logger.getLogger(Deployer.class);
 	
-	private NodePoolManager npm;
-	private ServiceDeployer deployer;
-
-	public Deployer() {
-		
-		npm = new NPMClient(Configuration.get(Option.NODE_POOL_MANAGER_URI));
-		deployer = new ServiceDeployerClient(Configuration.get(Option.SERVICE_DEPLOYER_URI));
-	}
-	
 	public Map<String, Service> deployServices(ChorSpec chor) {
 	
 		logger.info("Deploying services");
-		Map<String, Service> deployedServices = new HashMap<String, Service>(); // key is serviceName
-		
+
+		List<Thread> trds = new ArrayList<Thread>();
+		List<ServiceDeployerInvoker> invokers = new ArrayList<ServiceDeployerInvoker>();
 		for (ChorServiceSpec chorServiceSpec: chor.getServiceSpecs()) {
 			ServiceSpec serviceSpec = chorServiceSpec.getServiceSpec();
 			logger.debug("Requesting deploy of " + serviceSpec);
-			Service deployed;
-			try {
-				deployed = deployer.deploy(serviceSpec);
-				deployedServices.put(chorServiceSpec.getName(), deployed);
-			} catch (ServiceNotDeployedException e) {
-				logger.error(chorServiceSpec.getName() + " deploy has failed");
-			}
+			ServiceDeployerInvoker invoker = new ServiceDeployerInvoker(serviceSpec);
+			Thread trd = new Thread(invoker);
+			trds.add(trd);
+			invokers.add(invoker);
+			trd.start();
 		}
+		
+		waitThreads(trds);
 		
 		logger.info("Nodes are configured to receive services");
 		
-		for (Service svc: deployedServices.values()) {
-			String nodeId = svc.getNodeId();
+		trds = new ArrayList<Thread>();
+		Map<String, Service> deployedServices = new HashMap<String, Service>(); // key is serviceName
+		for (ServiceDeployerInvoker invoker: invokers) {
+			
+			Service deployed = invoker.deployed;
+			deployedServices.put(deployed.getName(), deployed);
+			
+			String nodeId = deployed.getNodeId();
+			NodeUpgrader upgrader = new NodeUpgrader(nodeId);
+			Thread trd = new Thread(upgrader);
+			trds.add(trd);
+			trd.start();
+		}
+		
+		waitThreads(trds);
+		
+		logger.info("Deployement finished");
+
+		return deployedServices;
+	}
+	
+	private void waitThreads(List<Thread> trds) {
+
+		for (Thread trd: trds) {
+			try {
+				trd.join();
+			} catch (InterruptedException e) {
+				logger.error("Error at thread join!", e);
+			}
+		}
+	}
+
+	private class ServiceDeployerInvoker implements Runnable {
+
+		ServiceDeployer deployer = new ServiceDeployerClient(Configuration.get(Option.SERVICE_DEPLOYER_URI));
+		ServiceSpec serviceSpec; // input
+		Service deployed; // output
+		
+		public ServiceDeployerInvoker(ServiceSpec serviceSpec) {
+			this.serviceSpec = serviceSpec;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				deployed = deployer.deploy(serviceSpec);
+			} catch (ServiceNotDeployedException e) {
+				logger.error(serviceSpec.getName() + " deploy has failed");
+			}			
+		}
+		
+	}
+	
+	private class NodeUpgrader implements Runnable {
+
+		NodePoolManager npm = new NPMClient(Configuration.get(Option.NODE_POOL_MANAGER_URI));
+		String nodeId;
+		
+		public NodeUpgrader(String nodeId) {
+			this.nodeId = nodeId;
+		}
+		
+		@Override
+		public void run() {
 			try {
 				npm.upgradeNode(nodeId);
 			} catch (NodeNotUpgradedException e) {
@@ -59,8 +115,6 @@ public class Deployer {
 				logger.error("Bad response from /nodes/" + nodeId + "/upgrade; maybe some service is not deployed");
 			}
 		}
-		logger.info("Deployement finished");
-
-		return deployedServices;
+		
 	}
 }
