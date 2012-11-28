332a39bcce69d6d84de19942a8625f1455698cc3
leonardofl <leonardofl87@gmail.com>
2012-11-28 14:17:59 -0200

    clean_chef.sh also cleans open stack entries

--- chef-repo/clean_chef.sh
+++ chef-repo/clean_chef.sh
@@ -4,7 +4,7 @@
 
 for node in `knife node list`
 do
-  if [ ${node:0:3} = 'ip-' -o ${node:0:5} = 'domU-' ]  
+  if [ ${node:0:3} = 'ip-' -o ${node:0:5} = 'domU-' -o ${node:0:8} = 'default-' ]  
   then
     knife node delete $node -y 
   fi
@@ -12,7 +12,7 @@ done
 
 for client in `knife client list`
 do
-  if [ ${client:0:3} = 'ip-' -o ${client:0:5} = 'domU-' ]  
+  if [ ${client:0:3} = 'ip-' -o ${client:0:5} = 'domU-' -o ${client:0:8} = 'default-' ]  
   then
     knife client delete $client -y 
   fi
