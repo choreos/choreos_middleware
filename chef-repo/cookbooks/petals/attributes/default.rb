946635c0a8951b0206d5d027bae9e7f1a8ea9a36
leonardofl <leonardofl87@gmail.com>
2013-02-08 02:26:57 -0200

    removendo cookbook petals

--- chef-repo/cookbooks/petals/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default['petals']['dir'] = '/opt/dsb-fulldistribution/dsb-distribution-1.0-SNAPSHOT'
-default['petals']['install_dir'] = '/opt/dsb-fulldistribution/dsb-distribution-1.0-SNAPSHOT/install'
-default['petals']['container_type'] = 'slave'
