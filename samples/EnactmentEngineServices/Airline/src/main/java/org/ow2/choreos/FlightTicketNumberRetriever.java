882eb4ba042ce3e48783dca54f9f0dca978b42ee
leonardofl <leonardofl87@gmail.com>
2013-03-11 16:03:19 -0300

    improving airline-response parsing

--- samples/EnactmentEngineServices/Airline/src/main/java/org/ow2/choreos/FlightTicketNumberRetriever.java
+++ samples/EnactmentEngineServices/Airline/src/main/java/org/ow2/choreos/FlightTicketNumberRetriever.java
@@ -9,10 +9,12 @@ import javax.xml.soap.SOAPBodyElement;
 import javax.xml.soap.SOAPConnection;
 import javax.xml.soap.SOAPConnectionFactory;
 import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPHeader;
 import javax.xml.soap.SOAPMessage;
 
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class FlightTicketNumberRetriever {
 	
@@ -27,35 +29,61 @@ public class FlightTicketNumberRetriever {
 		
 		if (this.endpoint == null)
 			throw new IllegalStateException();
-		
+
 		try {
 			
-	        SOAPConnectionFactory sfc = SOAPConnectionFactory.newInstance();
-	        SOAPConnection connection = sfc.createConnection();
-	
-	        MessageFactory mf = MessageFactory.newInstance();
-	        SOAPMessage sm = mf.createMessage();
-	
-	        SOAPEnvelope envelope = sm.getSOAPPart().getEnvelope();
-	        envelope.addNamespaceDeclaration("chor", "http://choreos.ow2.org/");
-	        
-	        SOAPHeader sh = sm.getSOAPHeader();
-	        SOAPBody sb = sm.getSOAPBody();
-	        sh.detachNode();
-	        QName bodyName = new QName("buyFlight");
-	        SOAPBodyElement bodyElement = sb.addBodyElement(bodyName);
-	        bodyElement.setPrefix("chor");
-	        
-	        SOAPMessage response = connection.call(sm, this.endpoint);
-	        
-	        Node returnNode = response.getSOAPBody().getChildNodes().item(0).getChildNodes().item(0);
-	        String flightTicketNumber = returnNode.getTextContent();
-	        
+	        SOAPMessage response = invokeService();
+	        String flightTicketNumber = parseResponse(response);
 	        return flightTicketNumber;
-		
 		} catch (Exception e) {
 			throw new IllegalStateException();
 		}
+	}
+
+	private SOAPMessage invokeService() throws SOAPException {
+		
+		SOAPConnectionFactory sfc = SOAPConnectionFactory.newInstance();
+		SOAPConnection connection = sfc.createConnection();
+
+		MessageFactory mf = MessageFactory.newInstance();
+		SOAPMessage sm = mf.createMessage();
+
+		SOAPEnvelope envelope = sm.getSOAPPart().getEnvelope();
+		envelope.addNamespaceDeclaration("chor", "http://choreos.ow2.org/");
+		
+		SOAPHeader sh = sm.getSOAPHeader();
+		SOAPBody sb = sm.getSOAPBody();
+		sh.detachNode();
+		QName bodyName = new QName("buyFlight");
+		SOAPBodyElement bodyElement = sb.addBodyElement(bodyName);
+		bodyElement.setPrefix("chor");
+		
+		SOAPMessage response = connection.call(sm, this.endpoint);
+		return response;
+	}
+
+	private String parseResponse(SOAPMessage response) throws SOAPException {
+		
+		NodeList bodyChildren = response.getSOAPBody().getChildNodes();
+		Node responseNode = null;
+		for (int i=0; i<bodyChildren.getLength(); i++) {
+			Node node = bodyChildren.item(i);
+			if (node.getNodeName().contains("buyFlightResponse")) {
+				responseNode = node;
+			}
+		}
+		
+		NodeList responseChildren = responseNode.getChildNodes();
+		Node returnNode = null;
+		for (int i=0; i<responseChildren.getLength(); i++) {
+			Node node = responseChildren.item(i);
+			if (node.getNodeName().contains("return") || node.getNodeName().contains("result")) {
+				returnNode = node;
+			}
+		}
+		
+		String flightTicketNumber = returnNode.getTextContent();
+		return flightTicketNumber;
 	}
 
 }
