6aab1262cb5673c58c315a5a061362dc4732e203
Thiago Furtado <thiagofurtado17@gmail.com>
2013-05-08 13:42:12 -0300

    Added new recipes to activate and deactivate services packaged as JAR files

--- DeploymentManager/src/main/resources/chef/service-deploy-recipe-template/recipes/jar.rb
+++ DeploymentManager/src/main/resources/chef/service-deploy-recipe-template/recipes/jar.rb
@@ -21,13 +21,15 @@ include_recipe "java"
 
 
 service "service_$NAME_jar" do
- 	start_command "start-stop-daemon -b --start --quiet --oknodo --pidfile /var/run/service-$NAME.pid --exec /bin/bash -- -c \"echo $$ > /var/run/service-$NAME.pid ; exec java -jar #{node['CHOReOSData']['serviceData']['$NAME']['InstallationDir']}/service-$NAME.jar\""
+	# Dirty trick to save the java pid to a file
+ 	start_command "start-stop-daemon -b --start --quiet --oknodo --pidfile /var/run/service-$NAME.pid --exec /bin/bash -- -c \"echo \\$\\$ > /var/run/service-$NAME.pid ; exec java -jar #{node['CHOReOSData']['serviceData']['$NAME']['InstallationDir']}/service-$NAME.jar\""
  	stop_command "start-stop-daemon --stop --signal 15 --quiet --oknodo --pidfile /var/run/service-$NAME.pid"
  	action :nothing
- 	supports :start => true, :stop => true
+	status_command "if ps -p `cat /var/run/service-$NAME.pid` > /dev/null  ; then exit 0; else exit 1; fi"
+ 	supports :start => true, :stop => true, :status => true
 end
 
-if node['CHOReOSData']['serviceData']['$NAME']['NumberOfClients'] > 0
+if node['CHOReOSData']['serviceData']['$NAME']['Active']
 	remote_file "#{node['CHOReOSData']['serviceData']['$NAME']['InstallationDir']}/service-$NAME.jar" do
   		source "#{node['CHOReOSData']['serviceData']['$NAME']['PackageURL']}"
   		action :create_if_missing
@@ -36,10 +38,22 @@ if node['CHOReOSData']['serviceData']['$NAME']['NumberOfClients'] > 0
 	end
 end
 
-if node['CHOReOSData']['serviceData']['$NAME']['NumberOfClients'] <= 0
+if not node['CHOReOSData']['serviceData']['$NAME']['Active']
 	file "#{node['CHOReOSData']['serviceData']['$NAME']['InstallationDir']}/service-$NAME.jar" do
 		notifies :stop, "service[service_$NAME_jar]", :immediately
 		#notifies :disable, "service[service_$NAME_jar]", :immediately
 		action :delete
 	end
 end
+
+
+
+
+# As the last resource in the dnsserver::remove_slave recipe, assuming that the remove_slave
+# "undoes" a dnsserver slave installation of some kind, without knowing what that might have been.
+#ruby_block "remove_this_recipe" do
+#  block do
+#    node.run_list.remove("recipe[dnsserver::remove_slave]") if node.run_list.include?("recipe[dnsserver::remove_slave]")
+#  end
+#  action :nothing
+#end
