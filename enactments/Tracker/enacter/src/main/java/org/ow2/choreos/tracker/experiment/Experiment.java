d83c98a3c0b5b47cdb22b0e70338bfe9eb6ae71e
leonardofl <leonardofl87@gmail.com>
2013-05-26 10:38:45 -0300

    tracker experiment with report

--- enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/Experiment.java
+++ enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/Experiment.java
@@ -1,5 +1,6 @@
 package org.ow2.choreos.tracker.experiment;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,12 +16,12 @@ import org.ow2.choreos.utils.LogConfigurator;
 
 public class Experiment {
 	
-	private static final int CHORS_QTY = 2;
-	private static final int CHORS_SIZE = 3;
+	private static final int CHORS_QTY = 3;
+	private static final int CHORS_SIZE = 4;
 	private static final int RUN = 1;
 	private static final String TRACKER_WAR_URL = "http://valinhos.ime.usp.br:54080/services/tracker.war";
 
-	private static final int ENACTMENT_TIMEOUT = 10;
+	private static final int ENACTMENT_TIMEOUT = 20;
 	private static final int VERIFY_TIMEOUT = 1;
 	
 	private static Logger logger = Logger.getLogger(Experiment.class);
@@ -35,41 +36,76 @@ public class Experiment {
 	public void run() {
 		
 		logger.info("Running " + RUN);
+		Report report = new Report(RUN);
+		
+		long t0_total = System.nanoTime();
 		
 		ExecutorService executor = Executors.newFixedThreadPool(CHORS_QTY);
 		List<RunnableEnacter> enacters = new ArrayList<RunnableEnacter>();
+		long t0 = System.nanoTime();
 		for (int i=0; i<CHORS_QTY; i++) {
 			Enacter enacter = new Enacter(i);
-			RunnableEnacter runnable = new RunnableEnacter(enacter);
+			RunnableEnacter runnable = new RunnableEnacter(enacter, report);
 			enacters.add(runnable);
 			executor.submit(runnable);
 		}
 		
 		Concurrency.waitExecutor(executor, ENACTMENT_TIMEOUT);
+		long tf = System.nanoTime();
+		report.setChorsEnactmentTotalTime(tf - t0);
 		
 		executor = Executors.newFixedThreadPool(CHORS_QTY);
+		List<RunnableVerifier> verifiers = new ArrayList<RunnableVerifier>();
+		t0 = System.nanoTime();
 		for (RunnableEnacter enacter: enacters) {
-			RunnableVerifier verifier = new RunnableVerifier(enacter.enacter);
+			RunnableVerifier verifier = new RunnableVerifier(enacter.enacter, report);
+			verifiers.add(verifier);
 			executor.submit(verifier);
 		}
 		
 		Concurrency.waitExecutor(executor, VERIFY_TIMEOUT);
+		tf = System.nanoTime();
+		report.setCheckTotalTime(tf - t0);
+		
+		long tf_total = System.nanoTime();
+		long delta_total = tf_total - t0_total;
+		report.setTotalTime(delta_total);
+		
+		int working = 0;
+		for (RunnableVerifier verifier: verifiers) {
+			if (verifier.ok) {
+				working++;
+			}
+		}
+		report.setChorsWorking(working);
+		
+		System.out.println(report);
+		try {
+			report.toFile();
+		} catch (IOException e) {
+			logger.error("Could not save the report.");
+		}
 	}
 
 	class RunnableEnacter implements Runnable {
 
 		Enacter enacter;
+		Report report;
 		boolean ok = true;
 		
-		RunnableEnacter(Enacter enacter) {
+		RunnableEnacter(Enacter enacter, Report report) {
 			this.enacter = enacter;
+			this.report = report;
 		}
 		
 		@Override
 		public void run() {
 			logger.info("Enacting Enacter#" + enacter.getId());
 			try {
+				long t0 = System.nanoTime();
 				enacter.enact(TRACKER_WAR_URL, CHORS_SIZE);
+				long tf = System.nanoTime();
+				report.addChorEnactmentTime(tf-t0);
 			} catch (MalformedURLException e) {
 				failed();
 			} catch (EnactmentException e) {
@@ -89,23 +125,28 @@ public class Experiment {
 	class RunnableVerifier implements Runnable {
 
 		Enacter enacter;
+		Report report;
 		boolean ok = true;
 		
-		RunnableVerifier(Enacter enacter) {
+		RunnableVerifier(Enacter enacter, Report report) {
 			this.enacter = enacter;
+			this.report = report;
 		}
+
 		
 		@Override
 		public void run() {
 			logger.info("Verifying Enacter#" + enacter.getId());
 			try {
+				long t0 = System.nanoTime();
 				ok = enacter.verifyAnswer();
-				logger.info("Enacter#" + enacter.getId() + " ok: " + ok);
+				long tf = System.nanoTime();
+				report.addCheckTime(tf-t0);
 			} catch (MalformedURLException e) {
 				logger.error("Ops, this problem should not occur with Enacter#" + enacter.getId());
 				ok = false;
 			}
-			logger.info("Enacter#" + enacter.getId() + " verified");
+			logger.info("Enacter#" + enacter.getId() + " ok: " + ok);
 		}
 		
 	}
--- enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/Report.java
+++ enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/Report.java
@@ -1,7 +1,134 @@
 package org.ow2.choreos.tracker.experiment;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Times must be set in nanoseconds
+ * 
+ * @author leonardo
+ *
+ */
 public class Report {
 
+	private static final double CONVERSOR = 1000000000.0;
 	
+	public int run;
 	
+	boolean calculated;
+	
+	int chorsQty;
+	int chorsSize;
+	List<Double> chorsEnactmentTimes = new ArrayList<Double>();
+	double chorsEnactmentMeanTime;
+	double chorsEnactmentStdDev;
+	double chorsEnactmentTotalTime;
+	
+	List<Double> checkTimes = new ArrayList<Double>();
+	double checkMeanTime;
+	double checkStdDev;
+	double checkTotalTime;
+	
+	int chorsWorking;
+	
+	double totalTime;
+	
+	public Report(int run) {
+		this.run = run;
+	}
+	
+	public void setChorsQuantity(int chorsQty) {
+		this.chorsQty = chorsQty;
+	}
+
+	public void setChorsWorking(int chorsWorking) {
+		this.chorsWorking = chorsWorking;
+	}
+
+	public void setTotalTime(long totalTime) {
+		this.totalTime = totalTime / CONVERSOR;
+	}
+	
+	public void setChorsEnactmentTotalTime(long chorsEnactmentTotalTime) {
+		this.chorsEnactmentTotalTime = chorsEnactmentTotalTime / CONVERSOR;
+	}
+
+	public void setCheckTotalTime(long checkTotalTime) {
+		this.checkTotalTime = checkTotalTime / CONVERSOR;
+	}
+
+	public synchronized void addChorEnactmentTime(long chorEnactmentTime) {
+		this.chorsEnactmentTimes.add(chorEnactmentTime / CONVERSOR);
+	}
+	
+	public synchronized void addCheckTime(long checkTime) {
+		this.checkTimes.add(checkTime / CONVERSOR);
+	}
+
+	private double mean(List<Double> values) {
+		
+		double sum = 0;
+		for (double v: values) {
+			sum += v;
+		}
+		return 1.0 * sum / values.size();
+	}
+	
+	private double stdDev(List<Double> values, double mean) {
+		
+		double sum = 0;
+		for (double v: values) {
+			sum += (v - mean) * (v - mean);
+		}
+		int n = values.size();
+		return  Math.sqrt(1.0 * sum / (n-1));
+	}
+
+	private void calculate() {
+
+		if (!this.calculated) {
+			
+			this.calculated = true;
+			
+			this.chorsEnactmentMeanTime = this.mean(this.chorsEnactmentTimes);
+			this.chorsEnactmentStdDev = this.stdDev(this.chorsEnactmentTimes, this.chorsEnactmentMeanTime);
+			this.checkMeanTime = this.mean(this.checkTimes);
+			this.checkStdDev = this.stdDev(this.checkTimes, this.checkMeanTime);
+		}
+	}
+	
+	public void toFile() throws IOException {
+		
+		String report = this.toString();
+		File file = new File("results/" + chorsQty + "x" + chorsSize + "_" + run + "run.txt");
+		Writer w = new FileWriter(file);
+		w.append(report + "\n");
+		w.close();
+	}
+
+	@Override
+	public String toString() {
+		this.calculate();
+		return "########Report:########## "
+				+ "\n // tuples are (mean, std dev)"
+				+ "\n // times in seconds"
+				+ "\n " + run + "/10 with " + chorsQty + " chors of size " + chorsSize + "; " + (new Date()).toString()
+				+ "\n How many choreographies to enact = " + chorsQty
+				+ "\n How many choreographies enacted = " + chorsEnactmentTimes.size()
+				+ "\n Time to enact choreographies = " + chorsEnactmentTimes
+				+ "\n Mean time to enact a choreography = (" + chorsEnactmentMeanTime + ", " + chorsEnactmentStdDev + ")"
+				+ "\n Total time to enact choreographies = " + chorsEnactmentTotalTime
+				+ "\n Time to check choreographies = " + checkTimes
+				+ "\n Mean time to check a choreography = (" + checkMeanTime + ", " + checkStdDev + ")"
+				+ "\n Total time to check choreographies = " + checkTotalTime
+				+ "\n How many choreographies working = " + chorsWorking
+				+ "\n Total time = " + totalTime + "\n";
+	}
+
+
 }
