5b376b019b9b9bbcd0b3262792760d9f38f6f5a3
leonardofl <leonardofl87@gmail.com>
2013-06-05 15:36:43 -0300

    deleting unused cookbooks

--- chef-repo/cookbooks/supermarket1/metadata.rb
+++
@@ -1,7 +0,0 @@
-depends          "petals"
-description      "Installs/Configures supermarket1"
-license          "LGPL 2.0 or, at your option, any later version"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-maintainer       "CCSL - Centro de Competencia de Software Livre - IME/USP"
-maintainer_email "ccsl@ime.usp.br"
-version          "0.0.7"
