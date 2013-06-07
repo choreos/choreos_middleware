c09aff7253c1fb519c4597c3bd9c297f3bb4b466
leonardofl <leonardofl87@gmail.com>
2013-06-07 10:59:16 -0300

    LimitedRoundRobinESBNodeSelector

---
+++ ChoreographyDeployer/src/main/java/org/ow2/choreos/chors/bus/ESBRegister.java
@@ -0,0 +1,19 @@
+package org.ow2.choreos.chors.bus;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ESBRegister {
+
+    private static List<EasyESBNode> esbNodes = new ArrayList<EasyESBNode>();
+    
+    public static synchronized void addEsbNode(EasyESBNode esbNode) {
+	esbNodes.add(esbNode);
+    }
+    
+    public static synchronized List<EasyESBNode> getEsbNodes() {
+	return Collections.unmodifiableList(esbNodes);
+    }
+    
+}
