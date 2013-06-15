c6eff40315ce577d329c598bfc5ff5a7ad078339
leonardofl <leonardofl87@gmail.com>
2013-06-15 15:07:12 -0300

    Invoker class

---
+++ Commons/src/main/java/org/ow2/choreos/breaker/Invoker.java
@@ -0,0 +1,63 @@
+package org.ow2.choreos.breaker;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.ow2.choreos.utils.Concurrency;
+
+/**
+ * Use this class to invoke external systems.
+ * 
+ * @author leonardo
+ * 
+ */
+public class Invoker<T> {
+
+    private Callable<T> task;
+    private int trials;
+    private int trialTimeout;
+    private TimeUnit timeoutUnit;
+
+    private Logger logger = Logger.getLogger(Invoker.class);
+
+    public Invoker(Callable<T> task, int trials, int trialTimeout, TimeUnit timeoutUnit) {
+	this.task = task;
+	this.trials = trials;
+	this.trialTimeout = trialTimeout;
+	this.timeoutUnit = timeoutUnit;
+    }
+
+    public T invoke() throws InvokerException {
+	Throwable cause = null;
+	for (int tried = 0; tried < trials; tried++) {
+	    try {
+		T result = tryToInvoke();
+		return result;
+	    } catch (Exception e) {
+		cause = e;
+	    }
+	}
+	throw new InvokerException(cause);
+    }
+
+    private T tryToInvoke() throws Exception {
+	ExecutorService executor = Executors.newSingleThreadExecutor();
+	Future<T> future = executor.submit(task);
+	Concurrency.waitExecutor(executor, trialTimeout, timeoutUnit, logger);
+	try {
+	    return Concurrency.checkFuture(future);
+	} catch (ExecutionException e) {
+	    Throwable cause = e.getCause();
+	    if (cause instanceof Exception)
+		throw (Exception) e;
+	    else
+		throw e;
+	}
+    }
+
+}
---
+++ Commons/src/test/java/org/ow2/choreos/breaker/InvokerTest.java
@@ -0,0 +1,112 @@
+package org.ow2.choreos.breaker;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+public class InvokerTest {
+
+    @Test
+    public void shouldInvoke() throws InvokerException {
+
+	Callable<String> task = new AlwaysWork();
+	int trials = 1;
+	int timeout = 1;
+
+	Invoker<String> invoker = new Invoker<String>(task, trials, timeout, TimeUnit.SECONDS);
+	String result = invoker.invoke();
+
+	assertEquals("0", result);
+    }
+
+    @Test
+    public void invocationShouldWorkInTheSecondTrial() throws InvokerException {
+
+	Callable<String> task = new WorksInTheSecond();
+	int trials = 2;
+	int timeout = 1;
+
+	Invoker<String> invoker = new Invoker<String>(task, trials, timeout, TimeUnit.SECONDS);
+	String result = invoker.invoke();
+
+	assertEquals("0", result);
+    }
+
+    @Test(expected = InvokerException.class)
+    public void invocationShouldNotWork() throws InvokerException {
+
+	Callable<String> task = new WorksInTheSecond();
+	int trials = 1;
+	int timeout = 1;
+
+	Invoker<String> invoker = new Invoker<String>(task, trials, timeout, TimeUnit.SECONDS);
+	invoker.invoke();
+    }
+
+    @Test
+    public void shouldInvokeAndNotTimeout() throws InvokerException {
+
+	Callable<String> task = new TakesTenMilliSecToWork();
+	int trials = 1;
+	int timeout = 20;
+
+	Invoker<String> invoker = new Invoker<String>(task, trials, timeout, TimeUnit.MILLISECONDS);
+	String result = invoker.invoke();
+
+	assertEquals("0", result);
+    }
+
+    @Test(expected = InvokerException.class)
+    public void shouldInvokeAndTimeout() throws InvokerException {
+
+	Callable<String> task = new TakesTenMilliSecToWork();
+	int trials = 1;
+	int timeout = 5;
+
+	Invoker<String> invoker = new Invoker<String>(task, trials, timeout, TimeUnit.MILLISECONDS);
+	invoker.invoke();
+    }
+
+    private class AlwaysWork implements Callable<String> {
+
+	AtomicInteger counter = new AtomicInteger();
+
+	@Override
+	public String call() throws Exception {
+	    return Integer.toString(counter.getAndIncrement());
+	}
+
+    }
+
+    private class WorksInTheSecond implements Callable<String> {
+
+	boolean first = true;
+	AtomicInteger counter = new AtomicInteger();
+
+	@Override
+	public String call() throws Exception {
+	    if (first) {
+		first = false;
+		throw new Exception();
+	    } else {
+		return Integer.toString(counter.getAndIncrement());
+	    }
+	}
+    }
+
+    private class TakesTenMilliSecToWork implements Callable<String> {
+
+	AtomicInteger counter = new AtomicInteger();
+
+	@Override
+	public String call() throws Exception {
+	    Thread.sleep(10);
+	    return Integer.toString(counter.getAndIncrement());
+	}
+
+    }
+}
