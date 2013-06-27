920c27b754a64b2939790fd0a9246a7866b70694
leonardofl <leonardofl87@gmail.com>
2013-06-27 08:53:21 -0300

    changing NPMFactory

--- DeploymentManager/src/main/java/org/ow2/choreos/deployment/nodes/NPMFactory.java
+++ DeploymentManager/src/main/java/org/ow2/choreos/deployment/nodes/NPMFactory.java
@@ -2,24 +2,18 @@ package org.ow2.choreos.deployment.nodes;
 
 import org.ow2.choreos.deployment.DeploymentManagerConfiguration;
 import org.ow2.choreos.deployment.nodes.cloudprovider.CloudProviderFactory;
-import org.ow2.choreos.nodes.NPMStub;
 import org.ow2.choreos.nodes.NodePoolManager;
 
 public class NPMFactory {
 
-    private static final String TESTING_PROPERTY = "TESTING";
     private static final String CLOUD_PROVIDER_PROPERTY = "CLOUD_PROVIDER";
 
-    /**
-     * The CloudProvider used is the one configured in the properties file. 
-     * If TESTING=true on properties, NPMStub is returned.
-     * 
-     * @return
-     */
+    public static NodePoolManager npmForTest;
+    public static boolean testing = false;
+    
     public static NodePoolManager getNewNPMInstance() {
-	Boolean testing = Boolean.parseBoolean(DeploymentManagerConfiguration.get(TESTING_PROPERTY));
 	if (testing) {
-	    return new NPMStub();
+	    return npmForTest;
 	} else {
 	    String cloudProviderType = DeploymentManagerConfiguration.get(CLOUD_PROVIDER_PROPERTY);
 	    return new NPMImpl(CloudProviderFactory.getInstance(cloudProviderType));
