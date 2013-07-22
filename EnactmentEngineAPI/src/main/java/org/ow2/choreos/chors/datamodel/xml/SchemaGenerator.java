b325f13ec2c8c29601170c788fa21a2043cb5bfc
leonardofl <leonardofl87@gmail.com>
2013-07-22 14:48:45 -0300

    changing SchemaGenerator to avoid Xerces usage

--- EnactmentEngineAPI/src/main/java/org/ow2/choreos/chors/datamodel/xml/SchemaGenerator.java
+++ EnactmentEngineAPI/src/main/java/org/ow2/choreos/chors/datamodel/xml/SchemaGenerator.java
@@ -5,19 +5,24 @@
 package org.ow2.choreos.chors.datamodel.xml;
 
 import java.io.IOException;
-import java.io.StringWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.SchemaOutputResolver;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.log4j.Logger;
-import org.apache.xml.serialize.OutputFormat;
-import org.apache.xml.serialize.XMLSerializer;
 import org.ow2.choreos.chors.datamodel.Choreography;
 import org.ow2.choreos.chors.datamodel.ChoreographySpec;
 import org.w3c.dom.Document;
@@ -26,35 +31,35 @@ public class SchemaGenerator {
 
     private Logger logger = Logger.getLogger(SchemaGenerator.class);
 
-    public String generateChorSpecSchema() {
-
+    public void printChorSpecSchema() {
         try {
-            return this.generateSchema(ChoreographySpec.class);
+            this.printSchema(ChoreographySpec.class);
         } catch (JAXBException e) {
-            logger.error("It should never happen");
-            return null;
+            logger.error("It should never happen", e);
         } catch (IOException e) {
-            logger.error("It should never happen");
-            return null;
+            logger.error("It should never happen", e);
+        } catch (TransformerException e) {
+            logger.error("It should never happen", e);
         }
     }
 
-    public String generateChoreographySchema() {
-
+    public void printChoreographySchema() {
         try {
-            return this.generateSchema(Choreography.class);
+            this.printSchema(Choreography.class);
         } catch (JAXBException e) {
-            logger.error("It should never happen");
-            return null;
+            logger.error("It should never happen", e);
         } catch (IOException e) {
-            logger.error("It should never happen");
-            return null;
+            logger.error("It should never happen", e);
+        } catch (TransformerException e) {
+            logger.error("It should never happen", e);
         }
     }
 
     // based on
     // http://arthur.gonigberg.com/2010/04/26/jaxb-generating-schema-from-object-model/
-    private String generateSchema(Class<?> clazz) throws JAXBException, IOException {
+    // and
+    // http://stackoverflow.com/questions/2325388/java-shortest-way-to-pretty-print-to-stdout-a-org-w3c-dom-document
+    private void printSchema(Class<?> clazz) throws JAXBException, IOException, TransformerException {
 
         // grab the context
         JAXBContext context = JAXBContext.newInstance(clazz);
@@ -78,20 +83,33 @@ public class SchemaGenerator {
         // output schema via System.out
         DOMResult domResult = results.get(0);
         Document doc = (Document) domResult.getNode();
-        OutputFormat format = new OutputFormat(doc);
-        format.setIndenting(true);
-        StringWriter writer = new StringWriter();
-        XMLSerializer serializer = new XMLSerializer(writer, format);
-        serializer.serialize(doc);
-        return writer.toString();
+        printDocument(doc, System.out);
+        
+//        OutputFormat format = new OutputFormat(doc);
+//        format.setIndenting(true);
+//        StringWriter writer = new StringWriter();
+//        XMLSerializer serializer = new XMLSerializer(writer, format);
+//        serializer.serialize(doc);
+//        return writer.toString();
+    }
+    
+    private static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+        transformer.transform(new DOMSource(doc), 
+             new StreamResult(new OutputStreamWriter(out, "UTF-8")));
     }
 
     public static void main(String[] args) {
-
         SchemaGenerator gen = new SchemaGenerator();
         System.out.println("ChorSpec XSD:");
-        System.out.println(gen.generateChorSpecSchema());
+        gen.printChorSpecSchema();
         System.out.println("\nChoreography XSD:");
-        System.out.println(gen.generateChoreographySchema());
+        gen.printChoreographySchema();
     }
 }
