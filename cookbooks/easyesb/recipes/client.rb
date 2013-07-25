f54a4b33ae1f0c862c5e96dbf0c7d16033e43781
leonardofl <leonardofl87@gmail.com>
2013-07-25 15:07:48 -0300

    updating easy esb client

--- cookbooks/easyesb/recipes/client.rb
+++ cookbooks/easyesb/recipes/client.rb
@@ -1,13 +1,6 @@
-#
-# Cookbook Name:: easyesb
-# Recipe:: client
-#
-# Downloads EasyESB client
-#
-# Copyright 2013, USP
-#
-# License MPL 2.0
-#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 
 include_recipe "apt" # java recipe is failing without recipe apt
 include_recipe "java"
