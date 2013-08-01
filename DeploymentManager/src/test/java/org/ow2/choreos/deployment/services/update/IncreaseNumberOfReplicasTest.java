e7794be148549d1ca27b6fd4545dc9e1608742fc
Thiago Furtado <thiagofurtado17@gmail.com>
2013-08-01 13:07:00 -0300

    Decreasing number of instances

--- DeploymentManager/src/test/java/org/ow2/choreos/deployment/services/update/IncreaseNumberOfReplicasTest.java
+++ DeploymentManager/src/test/java/org/ow2/choreos/deployment/services/update/IncreaseNumberOfReplicasTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.ow2.choreos.deployment.services.preparer.PrepareDeploymentFailedException;
@@ -23,60 +24,64 @@ public class IncreaseNumberOfReplicasTest {
     private ServiceDeploymentPreparer deploymentPreparer;
     private CloudNode node;
     private DeployableService airlineService;
-    
+
     @Before
     public void setUp() throws Exception {
-        setNode();
-        setPreparer();
-        setModels();
+	setNode();
+	setPreparer();
+	setModels();
     }
-    
+
     private void setNode() {
-        node = new CloudNode();
-        node.setId("unik");
-        node.setIp("192.168.56.101");
-        node.setUser("ubuntu");
-        node.setPrivateKey("ubuntu.pem");
+	node = new CloudNode();
+	node.setId("unik");
+	node.setIp("192.168.56.101");
+	node.setUser("ubuntu");
+	node.setPrivateKey("ubuntu.pem");
     }
-    
+
     private void setPreparer() throws PrepareDeploymentFailedException {
-        deploymentPreparer = mock(ServiceDeploymentPreparer.class);
-        when(deploymentPreparer.prepareDeployment()).thenReturn(Sets.newHashSet(node));
-        ServiceDeploymentPreparerFactory.preparerForTest = deploymentPreparer;
-        ServiceDeploymentPreparerFactory.testing = true;
+	deploymentPreparer = mock(ServiceDeploymentPreparer.class);
+	when(deploymentPreparer.prepareDeployment()).thenReturn(Sets.newHashSet(node));
+	ServiceDeploymentPreparerFactory.preparerForTest = deploymentPreparer;
+	ServiceDeploymentPreparerFactory.testing = true;
     }
-    
+
     private void setModels() {
-        ModelsForTest models = new ModelsForTest(ServiceType.SOAP, PackageType.COMMAND_LINE);
-        airlineService = models.getAirlineService();
+	ModelsForTest models = new ModelsForTest(ServiceType.SOAP, PackageType.COMMAND_LINE);
+	airlineService = models.getAirlineService();
+    }
+
+    @After
+    private void tearDown() {
+	ServiceDeploymentPreparerFactory.testing = false;
     }
 
     @Test
     public void test() throws UpdateActionFailedException {
 
-        DeployableServiceSpec newSpec = airlineService.getSpec().clone();
-        newSpec.setNumberOfInstances(2);
+	DeployableServiceSpec newSpec = airlineService.getSpec().clone();
+	newSpec.setNumberOfInstances(2);
 
-        assertEquals(1, airlineService.getSelectedNodes().size());
-        CloudNode originalNode = airlineService.getSelectedNodes().iterator().next(); 
-        
-        IncreaseNumberOfReplicas action = new IncreaseNumberOfReplicas(airlineService, newSpec);
-        action.applyUpdate();
-        
-        assertEquals(newSpec, airlineService.getSpec());
-        assertEquals(2, airlineService.getSelectedNodes().size());
-        int foundEquals = 0;
-        int foundDifferent = 0;
-        for (CloudNode node: airlineService.getSelectedNodes()) {
-            if (node.equals(originalNode))
-                foundEquals++;
-            else
-                foundDifferent++;
-        }
-        assertEquals(1, foundEquals);
-        assertEquals(1, foundDifferent);
-        
-        ServiceDeploymentPreparerFactory.testing = false;
+	assertEquals(1, airlineService.getSelectedNodes().size());
+	CloudNode originalNode = airlineService.getSelectedNodes().iterator().next();
+
+	IncreaseNumberOfReplicas action = new IncreaseNumberOfReplicas(airlineService, newSpec);
+	action.applyUpdate();
+
+	assertEquals(newSpec, airlineService.getSpec());
+	assertEquals(2, airlineService.getSelectedNodes().size());
+	int foundEquals = 0;
+	int foundDifferent = 0;
+	for (CloudNode node : airlineService.getSelectedNodes()) {
+	    if (node.equals(originalNode))
+		foundEquals++;
+	    else
+		foundDifferent++;
+	}
+	assertEquals(1, foundEquals);
+	assertEquals(1, foundDifferent);
+
     }
 
 }
