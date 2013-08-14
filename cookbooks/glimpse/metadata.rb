452b6e8db00b5cd6688e2154515217c4aca3dc96
Thiago Furtado <thiagofurtado17@gmail.com>
2013-08-14 17:59:26 -0300

    Added cookbooks for activemq and glimpse

---
+++ cookbooks/glimpse/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "CHOReOS"
+maintainer_email "ccsl@ime.usp.br"
+license          "MPL v2.0"
+description      "Install Glimpse into a cloud node"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "activemq"
+
---
+++ cookbooks/glimpse/recipes/default.rb
@@ -0,0 +1,83 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+
+include_recipe "activemq"
+
+settings_file = "systemSettings"
+glimpse_name = "glimpse-manager"
+glimpse_filename = "#{ENV['HOME']}/#{glimpse_name}"
+glimpse_tarballname = "#{glimpse_filename}.tar.gz"
+glimpse_install_path = "#{glimpse_filename}"
+
+remote_file "#{glimpse_tarballname}" do
+    source "#{node['glimpse']['tarball_url']}"
+	action :create_if_missing
+    not_if { ::File.exists?(glimpse_tarballname) }
+end
+
+home = "#{ENV['HOME']}"
+
+bash 'extract_glimpse_tarball' do
+  cwd ::File.dirname(home)
+  code <<-EOH
+    mkdir -p #{glimpse_filename}
+    tar xzf #{glimpse_tarballname} -C #{glimpse_install_path}
+    EOH
+  not_if { ::File.exists?(glimpse_install_path) }
+end
+
+drools = "configFiles/droolsFile"
+
+template "#{glimpse_install_path}/#{drools}" do
+  source "droolsFile.erb"
+  variables({
+	:probeTopic => "probeTopic"
+  })
+end
+
+environment = "configFiles/environmentFile"
+
+template "#{glimpse_install_path}/#{environment}" do
+  source "environmentFile.erb"
+  variables({
+	 :factory_initial => "org.apache.activemq.jndi.ActiveMQInitialContextFactory",
+	 :provider_url => "tcp://localhost:61616",
+	 :security_principal => "system",
+	 :credentials => "manager",
+	 :connection_factory => "TopicCF",
+	 :service_topic => "jms.serviceTopic",
+	 :probe_topic => "jms.probeTopic"
+  })
+end
+
+manager = "configFiles/managerFile"
+
+template "#{glimpse_install_path}/#{manager}" do
+  source "managerFile.erb"
+  variables({
+     :serviceTopic => "serviceTopic",
+	 :probeTopic => "probeTopic",
+	 :answerTopic => "answerTopic"
+  })
+end
+
+template "#{glimpse_install_path}/#{settings_file}" do
+  source "systemSettings.erb"
+  variables({
+	 :environment_file => "#{glimpse_install_path}/#{environment}",
+	 :drools_file => "#{glimpse_install_path}/#{drools}",
+	 :manager_file => "#{glimpse_install_path}/#{manager}"
+  })
+end
+
+service "#{glimpse_name}" do
+        # Dirty trick to save the java pid to a file
+        start_command "start-stop-daemon -b --start --quiet --oknodo --pidfile /var/run/#{glimpse_name}.pid --exec /bin/bash -- -c \"echo \\$\\$ > /var/run/#{glimpse_name}.pid ; exec java -jar #{glimpse_install_path}/#{glimpse_name}.jar #{glimpse_install_path}/#{settings_file}\""
+        stop_command "start-stop-daemon --stop --signal 15 --quiet --oknodo --pidfile /var/run/#{glimpse_name}.pid"
+        action :start
+        status_command "if ps -p `cat /var/run/#{glimpse_name}.pid` > /dev/null  ; then exit 0; else exit 1; fi"
+        supports :start => true, :stop => true, :status => true
+end
+
+
