7ec56191617e060a03ccaf2e27b4d801dfbd5ed6
leonardofl <leonardofl87@gmail.com>
2013-08-17 10:32:05 -0300

    TrackerProperties.getPackageUri

--- enactments/Tracker/webservice/src/main/java/org/ow2/choreos/tracker/TrackerProperties.java
+++ enactments/Tracker/webservice/src/main/java/org/ow2/choreos/tracker/TrackerProperties.java
@@ -1,46 +1,60 @@
 package org.ow2.choreos.tracker;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrackerProperties {
 
-	private static final String NAME_PREFIX = "tracker";
-	private static final String BLACK_LP = "TrackerBlackService";
-	private static final String WHITE_LP = "TrackerWhiteService";
+    private static final String NAME_PREFIX = "tracker";
+    private static final String BLACK_LP = "TrackerBlackService";
+    private static final String WHITE_LP = "TrackerWhiteService";
+    private static final Map<TrackerType,String> PACKAGE_URIS = new HashMap<TrackerType,String>();
 
-	public String getName(final int trackerNumber) {
-		return NAME_PREFIX + trackerNumber;
-	}
+    static {
+        PACKAGE_URIS.put(TrackerType.WHITE, "http://...");
+        PACKAGE_URIS.put(TrackerType.BLACK, "http://...");
+    }
+    
+    public String getName(final int trackerNumber) {
+        return NAME_PREFIX + trackerNumber;
+    }
 
-	public String getEndpoint(final TrackerType type) {
-		String endpoint;
+    public String getEndpoint(final TrackerType type) {
+        String endpoint;
 
-		if (TrackerType.WHITE.equals(type)) {
-			endpoint = "white";
-		} else {
-			endpoint = "black";
-		}
+        if (TrackerType.WHITE.equals(type)) {
+            endpoint = "white";
+        } else {
+            endpoint = "black";
+        }
 
-		return endpoint;
-	}
+        return endpoint;
+    }
 
-	public TrackerType getType(final int trackerNumber) {
-		TrackerType type;
-		if (trackerNumber % 5 == 4) {
-			type = TrackerType.BLACK;
-		} else {
-			type = TrackerType.WHITE;
-		}
+    public TrackerType getType(final int trackerNumber) {
+        TrackerType type;
+        if (trackerNumber % 5 == 4) {
+            type = TrackerType.BLACK;
+        } else {
+            type = TrackerType.WHITE;
+        }
 
-		return type;
-	}
+        return type;
+    }
+    
+    public String getPackageUri(final int trackerNumber) {
+        TrackerType type = getType(trackerNumber);
+        return PACKAGE_URIS.get(type);
+    }
 
-	public String getLocalPart(final TrackerType type) {
-		String localPart;
-		if (TrackerType.WHITE.equals(type)) {
-			localPart = WHITE_LP;
-		} else {
-			localPart = BLACK_LP;
-		}
+    public String getLocalPart(final TrackerType type) {
+        String localPart;
+        if (TrackerType.WHITE.equals(type)) {
+            localPart = WHITE_LP;
+        } else {
+            localPart = BLACK_LP;
+        }
 
-		return localPart;
-	}
+        return localPart;
+    }
 }
