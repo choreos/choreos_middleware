268a9e53cee9f25ea19e705d0eb68275b949d950
leonardofl <leonardofl87@gmail.com>
2013-10-04 17:56:22 -0300

    Alarm with mp3 filter and correct file name encoding

--- Commons/src/main/java/org/ow2/choreos/utils/Alarm.java
+++ Commons/src/main/java/org/ow2/choreos/utils/Alarm.java
@@ -1,26 +1,40 @@
 package org.ow2.choreos.utils;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.Random;
 
+import org.eclipse.jetty.util.URIUtil;
+
 public class Alarm {
-    
-    private static final String SONGS_FOLDER = System.getProperty("user.home") + "/Music"; 
+
+    private static final String SONGS_FOLDER = System.getProperty("user.home") + "/Music";
 
     public void play() {
+        FileFilter filter = new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().endsWith(".mp3");
+            }
+        };
         File folder = new File(SONGS_FOLDER);
-        File[] songs = folder.listFiles();
+        File[] songs = folder.listFiles(filter);
         int numberOfSongs = songs.length;
         Random random = new Random();
         int songIndex = random.nextInt(numberOfSongs);
         File song = songs[songIndex];
-        String commandStr = "vlc file://" + song.getAbsolutePath().replaceAll(" ", "%20");
-        OSCommand command = new OSCommand(commandStr);
+        String songFileName = URIUtil.encodePath(song.getAbsolutePath());
+        String commandStr = "vlc file://" + songFileName;
         try {
+            OSCommand command = new OSCommand(commandStr);
             command.execute();
         } catch (CommandLineException e) {
             System.out.println("Alarm!");
         }
     }
-    
+
+    public static void main(String[] args) {
+        Alarm a = new Alarm();
+        a.play();
+    }
 }
