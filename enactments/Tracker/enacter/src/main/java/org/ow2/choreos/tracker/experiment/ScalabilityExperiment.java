e162ec273c7af590dc23aec95af67be2c9d9e33e
leonardofl <leonardofl87@gmail.com>
2013-10-09 19:22:46 -0300

    ScalabilityExperiment working

--- enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/ScalabilityExperiment.java
+++ enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/ScalabilityExperiment.java
@@ -4,38 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+import javax.ws.rs.core.MediaType;
+
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.log4j.Logger;
 import org.ow2.choreos.invoker.Invoker;
 import org.ow2.choreos.invoker.InvokerBuilder;
 import org.ow2.choreos.invoker.InvokerException;
 import org.ow2.choreos.utils.CommandLineException;
+import org.ow2.choreos.utils.LogConfigurator;
 import org.ow2.choreos.utils.OSCommand;
 
 public class ScalabilityExperiment {
 
-    public static final int NUM_EXECUTIONS = 10;
+    public static final int NUM_EXECUTIONS = 2;
     public static final int CHORS_QUANTITY = 1;
-    public static final int[] CHORS_SIZES = new int[] { 200, 600, 1000, 1400, 1800 };
-    public static final int[] VMS_LIMITS = new int[] { 10, 30, 50, 70, 90 };
+    public static final int[] CHORS_SIZES = new int[] { 5, 10 };
+    public static final int[] VMS_LIMITS = new int[] { 1, 2 };
+//    public static final int[] CHORS_SIZES = new int[] { 200, 600, 1000, 1400, 1800 };
+//    public static final int[] VMS_LIMITS = new int[] { 10, 30, 50, 70, 90 };
 
-    private static final int TIME_TO_WAIT_EE_START_MILLISEC = 2 * 60 * 1000;
-    private static final String CHOREOS_MIDDLEWARE_FOLDER = "home/ubuntu/choreos_middleware";
+    private static final int TIME_TO_WAIT_EE_START_MILLISEC = 1 * 60 * 1000;
+    private static final String CHOREOS_MIDDLEWARE_FOLDER = "/home/leonardo/workspaces/choreos/choreos_middleware";
 
     private static Logger logger = Logger.getLogger(ScalabilityExperiment.class);
 
     private List<ExperimentDefinition> definitions;
     private OSCommand dmCommand, cdCommand;
+    private ExperimentDefinition currentDefinition;
 
     public static void main(String[] args) {
+        LogConfigurator.configLog();
         ScalabilityExperiment exps = new ScalabilityExperiment();
         exps.run();
     }
 
     private void run() {
+        Report.setRepetitions(NUM_EXECUTIONS);
         createDefinitions();
-        for (int i = 0; i < NUM_EXECUTIONS; i++) {
-            executeDefinition(definitions.get(i));
+        for (ExperimentDefinition def : definitions) {
+            currentDefinition = def;
+            executeDefinition(def);
         }
+        cleanAmazon();
     }
 
     private void createDefinitions() {
@@ -44,7 +55,7 @@ public class ScalabilityExperiment {
             for (int i = 0; i < NUM_EXECUTIONS; i++) {
                 int size = CHORS_SIZES[s];
                 int vmsLimit = VMS_LIMITS[s];
-                definitions.add(new ExperimentDefinition(i, CHORS_QUANTITY, size, vmsLimit));
+                definitions.add(new ExperimentDefinition(i+1, CHORS_QUANTITY, size, vmsLimit));
             }
         }
     }
@@ -53,13 +64,14 @@ public class ScalabilityExperiment {
         logger.info("Executing definition " + def);
         cleanAmazon();
         startEE();
+        configureEE();
         Experiment exp = new Experiment(def);
-        exp.run(); 
+        exp.run();
         stopEE();
     }
 
     private void cleanAmazon() {
-        OSCommand command = new OSCommand("." + CHOREOS_MIDDLEWARE_FOLDER + "/scripts/clean_aws.sh");
+        OSCommand command = new OSCommand("sh " + CHOREOS_MIDDLEWARE_FOLDER + "/scripts/clean_aws.sh");
         CommandTask task = new CommandTask(command);
         Invoker<String> invoker = new InvokerBuilder<String>(task, 120).trials(5).pauseBetweenTrials(30).build();
         try {
@@ -68,10 +80,12 @@ public class ScalabilityExperiment {
             logger.error("Could not kill EC2 instances!!!");
             System.exit(-1);
         }
+        logger.info("EC2 instances destroyed");
     }
 
     private void startEE() {
-        String mvnExecCom = "mvn:exec";
+        logger.info("Starting EE...");
+        String mvnExecCom = "mvn exec:java -o";
         try {
             dmCommand = new OSCommand(mvnExecCom, CHOREOS_MIDDLEWARE_FOLDER + "/DeploymentManager");
             dmCommand.executeAssync();
@@ -85,11 +99,23 @@ public class ScalabilityExperiment {
         } catch (InterruptedException e) {
             logger.error("Killed while sleeping!");
         }
+        logger.info("EE started (I hope)");
+    }
+
+    private void configureEE() {
+        String host = "http://0.0.0.0:9100/deploymentmanager";
+        WebClient client = WebClient.create(host);
+        client.type(MediaType.TEXT_PLAIN);
+        client.path("nodes").path("vm_limit");
+        String vmLimit = Integer.toString(currentDefinition.getVmLimit());
+        client.put(vmLimit);
+        logger.info("EE configured");
     }
 
     private void stopEE() {
         dmCommand.killProcess();
         cdCommand.killProcess();
+        logger.info("EE killed");
     }
 
     private class CommandTask implements Callable<String> {
