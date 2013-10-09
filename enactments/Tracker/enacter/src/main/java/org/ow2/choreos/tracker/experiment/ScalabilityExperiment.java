46a54d0ae3beffe997411354d92321a4d0bdcade
leonardofl <leonardofl87@gmail.com>
2013-10-09 17:17:51 -0300

    continuing with ScalabilityExperiment

--- enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/ScalabilityExperiment.java
+++ enactments/Tracker/enacter/src/main/java/org/ow2/choreos/tracker/experiment/ScalabilityExperiment.java
@@ -12,34 +12,36 @@ import org.ow2.choreos.utils.CommandLineException;
 import org.ow2.choreos.utils.OSCommand;
 
 public class ScalabilityExperiment {
-    
+
     public static final int NUM_EXECUTIONS = 10;
     public static final int CHORS_QUANTITY = 1;
-    public static final int[] CHORS_SIZES = new int[]{200, 600, 1000, 1400, 1800};
-    public static final int[] VMS_LIMITS = new int[]{10, 30, 50, 70, 90};
+    public static final int[] CHORS_SIZES = new int[] { 200, 600, 1000, 1400, 1800 };
+    public static final int[] VMS_LIMITS = new int[] { 10, 30, 50, 70, 90 };
 
+    private static final int TIME_TO_WAIT_EE_START_MILLISEC = 2 * 60 * 1000;
     private static final String CHOREOS_MIDDLEWARE_FOLDER = "home/ubuntu/choreos_middleware";
-    
+
     private static Logger logger = Logger.getLogger(ScalabilityExperiment.class);
-    
+
     private List<ExperimentDefinition> definitions;
-    
+    private OSCommand dmCommand, cdCommand;
+
     public static void main(String[] args) {
         ScalabilityExperiment exps = new ScalabilityExperiment();
         exps.run();
     }
-    
+
     private void run() {
         createDefinitions();
-        for (int i=0; i<NUM_EXECUTIONS; i++) {
+        for (int i = 0; i < NUM_EXECUTIONS; i++) {
             executeDefinition(definitions.get(i));
         }
     }
 
     private void createDefinitions() {
         definitions = new ArrayList<ExperimentDefinition>();
-        for (int s=0; s<CHORS_SIZES.length; s++) {
-            for (int i=0; i<NUM_EXECUTIONS; i++) {
+        for (int s = 0; s < CHORS_SIZES.length; s++) {
+            for (int i = 0; i < NUM_EXECUTIONS; i++) {
                 int size = CHORS_SIZES[s];
                 int vmsLimit = VMS_LIMITS[s];
                 definitions.add(new ExperimentDefinition(i, CHORS_QUANTITY, size, vmsLimit));
@@ -52,42 +54,48 @@ public class ScalabilityExperiment {
         cleanAmazon();
         startEE();
         Experiment exp = new Experiment(def);
-        exp.run(); // TODO set timeout
+        exp.run(); 
         stopEE();
     }
 
     private void cleanAmazon() {
         OSCommand command = new OSCommand("." + CHOREOS_MIDDLEWARE_FOLDER + "/scripts/clean_aws.sh");
         CommandTask task = new CommandTask(command);
-        Invoker<String> invoker = new InvokerBuilder<String>(task, 120).trials(3).build();
+        Invoker<String> invoker = new InvokerBuilder<String>(task, 120).trials(5).pauseBetweenTrials(30).build();
         try {
             invoker.invoke();
         } catch (InvokerException e) {
             logger.error("Could not kill EC2 instances!!!");
+            System.exit(-1);
         }
     }
 
     private void startEE() {
         String mvnExecCom = "mvn:exec";
-        // TODO: execute each one of these commands in a new thread, so we do not get blocked
         try {
-            OSCommand dmCommand = new OSCommand(mvnExecCom, CHOREOS_MIDDLEWARE_FOLDER + "/DeploymentManager");
-            dmCommand.execute();
-            OSCommand cdCommand = new OSCommand(mvnExecCom, CHOREOS_MIDDLEWARE_FOLDER + "/ChoreographyDeployer");
-            cdCommand.execute();
+            dmCommand = new OSCommand(mvnExecCom, CHOREOS_MIDDLEWARE_FOLDER + "/DeploymentManager");
+            dmCommand.executeAssync();
+            cdCommand = new OSCommand(mvnExecCom, CHOREOS_MIDDLEWARE_FOLDER + "/ChoreographyDeployer");
+            cdCommand.executeAssync();
         } catch (CommandLineException e) {
             logger.error("Could not start EE");
+        }
+        try {
+            Thread.sleep(TIME_TO_WAIT_EE_START_MILLISEC);
+        } catch (InterruptedException e) {
+            logger.error("Killed while sleeping!");
         }
     }
 
     private void stopEE() {
-        // ./kill_ee.sh
+        dmCommand.killProcess();
+        cdCommand.killProcess();
     }
-    
+
     private class CommandTask implements Callable<String> {
 
         OSCommand command;
-        
+
         public CommandTask(OSCommand command) {
             this.command = command;
         }
