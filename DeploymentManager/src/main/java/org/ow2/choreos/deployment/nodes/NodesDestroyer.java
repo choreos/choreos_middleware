143cdd7da16ce5e6d5d08ce4564d89e0c49c9c8c
leonardofl <leonardofl87@gmail.com>
2013-10-10 16:00:16 -0300

    timeout to destroy all nodes

--- DeploymentManager/src/main/java/org/ow2/choreos/deployment/nodes/NodesDestroyer.java
+++ DeploymentManager/src/main/java/org/ow2/choreos/deployment/nodes/NodesDestroyer.java
@@ -3,6 +3,8 @@ package org.ow2.choreos.deployment.nodes;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.ow2.choreos.nodes.NodeNotDestroyed;
 import org.ow2.choreos.nodes.datamodel.CloudNode;
@@ -10,12 +12,15 @@ import org.ow2.choreos.utils.Concurrency;
 
 public class NodesDestroyer {
 
+    private static final int TIMEOUT_MINUTES = 3;
+    
     private Collection<CloudNode> nodesToDestroy;
     private NodeDestroyerFactory nodeDestroyerFactory;
     private NodeRegistry nodeRegistry;
-    private List<Thread> trds = new ArrayList<Thread>();
-    private List<NodeDestroyer> destroyers = new ArrayList<NodeDestroyer>();
 
+    private List<NodeDestroyer> destroyers = new ArrayList<NodeDestroyer>();
+    private ExecutorService executor;
+    
     public NodesDestroyer(Collection<CloudNode> nodesToDestroy, NodeDestroyerFactory nodeDestroyerFactory) {
         this.nodesToDestroy = nodesToDestroy;
         this.nodeDestroyerFactory= nodeDestroyerFactory; 
@@ -29,19 +34,18 @@ public class NodesDestroyer {
     }
 
     private void destroy() {
-        trds = new ArrayList<Thread>();
+        executor = Executors.newFixedThreadPool(nodesToDestroy.size());
         destroyers = new ArrayList<NodeDestroyer>();
         for (CloudNode node : nodesToDestroy) {
             NodeDestroyer destroyer = nodeDestroyerFactory.getNewInstance(node);
-            Thread trd = new Thread(destroyer);
             destroyers.add(destroyer);
-            trds.add(trd);
-            trd.start();
+            executor.submit(destroyer);
         }
     }
 
     private void waitDestroy() {
-        Concurrency.waitThreads(trds); // FIXME
+        String erMsg = "Could not wait for nodes destroyment";
+        Concurrency.waitExecutor(executor, TIMEOUT_MINUTES, erMsg);
     }
 
     private void checkDestroyed() throws NodeNotDestroyed {
