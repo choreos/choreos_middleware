5105f324325079c98445d452b93e33ac77a05e69
leonardofl <leonardofl87@gmail.com>
2013-10-22 15:06:00 -0200

    improving IdlePoolFactory

--- DeploymentManager/src/main/java/org/ow2/choreos/deployment/nodes/IdlePoolFactory.java
+++ DeploymentManager/src/main/java/org/ow2/choreos/deployment/nodes/IdlePoolFactory.java
@@ -1,11 +1,18 @@
 package org.ow2.choreos.deployment.nodes;
 
+import org.apache.log4j.Logger;
 import org.ow2.choreos.deployment.DeploymentManagerConfiguration;
 
+
 public class IdlePoolFactory {
+    
+    private static final int DEFAULT_POOL_SIZE = 0;
+    private static final int DEFAULT_POOL_THRESHOLD = -1;
 
     public static IdlePool idlePoolForTesting;
     public static boolean testing;
+    
+    private static Logger logger = Logger.getLogger(IdlePoolFactory.class);
 
     public IdlePool getNewIdlePool() {
         if (testing) {
@@ -16,29 +23,21 @@ public class IdlePoolFactory {
     }
 
     private IdlePool loadPool() {
-        int poolSize = getPoolSize();
-        int threshold = getThreshold();
+        int poolSize = getValue("IDLE_POOL_INITIAL_SIZE", DEFAULT_POOL_SIZE);
+        int threshold = getValue("IDLE_POOL_THRESHOLD", DEFAULT_POOL_THRESHOLD);
         return IdlePool.getInstance(poolSize, threshold);
     }
 
-    private int getPoolSize() {
-        int poolSize = 0;
+    private int getValue(String property, int defaultValue) {
+        int value = defaultValue;
         try {
-            poolSize = Integer.parseInt(DeploymentManagerConfiguration.get("IDLE_POOL_INITIAL_SIZE"));
+            value = Integer.parseInt(DeploymentManagerConfiguration.get(property));
         } catch (NumberFormatException e) {
-            ; // no problem, poolSize is zero
+            logger.warn(property + " not integer. Going to use default " + defaultValue);
+        } catch (IllegalArgumentException e) {
+            logger.warn(property + " not found. Going to use default " + defaultValue);
         }
-        return poolSize;
-    }
-    
-    private int getThreshold() {
-        int threshold = -1;
-        try {
-            threshold = Integer.parseInt(DeploymentManagerConfiguration.get("IDLE_POOL_THRESHOLD"));
-        } catch (NumberFormatException e) {
-            ; // no problem, threshold is -1
-        }
-        return threshold;
+        return value;
     }
     
 }
